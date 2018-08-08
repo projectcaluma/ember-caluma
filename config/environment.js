b11bc297d42ccba15a122b415cc24fa071c86d42
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2018-08-08 14:10:53 +0200

    Initial commit

---
+++ config/environment.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = function(environment) {
+  const ENV = {
+    modulePrefix: "ember-caluma-form-builder",
+    environment
+  };
+
+  return ENV;
+};
---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/* eslint-disable node/no-unpublished-require */
+const EngineAddon = require("ember-engines/lib/engine-addon");
+
+module.exports = EngineAddon.extend({
+  name: "ember-caluma-form-builder",
+  lazyLoading: true
+});
