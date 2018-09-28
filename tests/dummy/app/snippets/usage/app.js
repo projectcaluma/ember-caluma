72122bdcbe5e4e7db4d5e4e374f770c21ab5086c
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2018-09-28 12:54:23 +0200

    Cleanup leftovers and update docs

--- tests/dummy/app/snippets/usage/app.js
+++ tests/dummy/app/snippets/usage/app.js
@@ -6,7 +6,8 @@ const App = Application.extend({
       dependencies: {
         services: [
           "apollo", // ember-apollo-client for graphql
-          "notification" // ember-uikit for notifications
+          "notification", // ember-uikit for notifications
+          "router" // ember router for navigation
         ]
       }
     }
