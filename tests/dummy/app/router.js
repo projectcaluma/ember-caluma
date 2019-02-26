7d77c409dcfae1c2f931c23d112f0bd39596a021
Christian Zosel <christian@zosel.ch>
2019-02-26 10:45:55 +0100

    Add maintenance section to docs

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -9,6 +9,7 @@ const Router = AddonDocsRouter.extend({
 Router.map(function() {
   docsRoute(this, function() {
     this.route("usage");
+    this.route("maintenance");
   });
 
   this.route("demo", function() {
