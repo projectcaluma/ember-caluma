0340314c049753f10810f1599cc4c155da66b47a
Christian Zosel <christian@zosel.ch>
2019-02-27 13:17:44 +0100

    Add `transform` utility for GraphQL responses

--- addon/utils/apollo.js
+++ addon/utils/apollo.js
@@ -1,21 +1,33 @@
-export function decodeMeta(obj) {
-  if (!obj) {
+export function transform(mapping, obj) {
+  function walk(obj) {
+    if (!obj) {
+      return obj;
+    }
+    if (Array.isArray(obj)) {
+      return obj.map(walk);
+    }
+    if (typeof obj === "object") {
+      return Object.keys(obj).reduce((result, key) => {
+        if (key.startsWith("_")) {
+          result[key] = obj[key];
+        } else if (Object.keys(mapping).includes(key)) {
+          result[key] = mapping[key](obj[key]);
+        } else {
+          result[key] = walk(obj[key]);
+        }
+        return result;
+      }, {});
+    }
     return obj;
   }
-  if (Array.isArray(obj)) {
-    return obj.map(decodeMeta);
-  }
-  if (typeof obj === "object") {
-    return Object.keys(obj).reduce((result, key) => {
-      if (key === "meta") {
-        result[key] = JSON.parse(obj[key]);
-      } else if (key.startsWith("_")) {
-        result[key] = obj[key];
-      } else {
-        result[key] = decodeMeta(obj[key]);
-      }
-      return result;
-    }, {});
-  }
-  return obj;
+  return walk(obj);
+}
+
+export function decodeMeta(obj) {
+  return transform(
+    {
+      meta: str => JSON.parse(str)
+    },
+    obj
+  );
 }
