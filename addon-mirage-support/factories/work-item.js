01d1f9904a81ab12652a137fa44a50bfcc381a94
Christian Zosel <christian@zosel.ch>
2019-03-01 07:35:43 +0100

    Add test for nested data fetching (work item)

--- addon-mirage-support/factories/work-item.js
+++ addon-mirage-support/factories/work-item.js
@@ -5,6 +5,8 @@ const STATUS = ["READY", "CANCELED", "COMPLETED"];
 export default Factory.extend({
   createdByUser: faker.random.uuid(),
   createdAt: faker.date.past(),
+  deadline: faker.date.future(),
   status: faker.list.random(...STATUS),
+  // eslint-disable-next-line ember/avoid-leaking-state-in-ember-objects
   addressedGroups: ["group1", "group2"]
 });
