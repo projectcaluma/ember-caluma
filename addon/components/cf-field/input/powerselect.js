c0a5850bb1eacb3eec2fd0ec255e32303c998e9c
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2019-03-05 16:45:16 +0100

    REFACTOR text by moving them to translations
    Moves the texts from the template to the translations and
    adds a computed property for the default placeholder text
    as that has to be different for single and multiple choice.

--- addon/components/cf-field/input/powerselect.js
+++ addon/components/cf-field/input/powerselect.js
@@ -1,10 +1,12 @@
 import Component from "@ember/component";
 import layout from "../../../templates/components/cf-field/input/powerselect";
 import { computed } from "@ember/object";
+import { inject as service } from "@ember/service";
 
 export default Component.extend({
   layout,
   tagName: "",
+  intl: service(),
 
   multiple: computed("field.question.__typename", function() {
     return this.get("field.question.__typename").startsWith("Multiple");
@@ -31,8 +33,10 @@ export default Component.extend({
     return name;
   }),
 
-  searchEnabled: computed("multiple", "choices", function() {
-    return this.get("choices").length > 3;
+  placeholder: computed("multiple", function() {
+    let suffix = this.get("multiple") ? "multiple" : "single";
+    let path = `caluma.form.power-select.placeholder-${suffix}`;
+    return this.get("intl").t(path);
   }),
 
   actions: {
