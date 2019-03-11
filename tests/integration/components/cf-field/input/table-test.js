53e95142ddeacc4a9134c836965832055c7655e3
Christian Zosel <christian@zosel.ch>
2019-03-11 14:39:03 +0100

    Add test

--- tests/integration/components/cf-field/input/table-test.js
+++ tests/integration/components/cf-field/input/table-test.js
@@ -7,11 +7,41 @@ module("Integration | Component | cf-field/input/table", function(hooks) {
   setupRenderingTest(hooks);
 
   test("it renders", async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    this.set("field", {
+      id: "table-test",
+      answer: {
+        rowDocuments: [
+          {
+            fields: [
+              { answer: { value: "Max" } },
+              { answer: { value: "Muster" } }
+            ]
+          },
+          {
+            fields: [
+              { answer: { value: "Bea" } },
+              { answer: { value: "Beispiel" } }
+            ]
+          }
+        ]
+      },
+      question: {
+        rowForm: {
+          questions: {
+            edges: [
+              { node: { label: "First name" } },
+              { node: { label: "Last name" } }
+            ]
+          }
+        }
+      }
+    });
 
-    await render(hbs`{{cf-field/input/table}}`);
+    await render(hbs`{{cf-field/input/table field=field}}`);
+    assert.dom("th:nth-of-type(1)").hasText("First name");
+    assert.dom("th:nth-of-type(2)").hasText("Last name");
 
-    assert.equal(this.element.textContent.trim(), "");
+    assert.dom("tbody > tr > td:nth-of-type(1)").hasText("Max");
+    assert.dom("tbody > tr > td:nth-of-type(2)").hasText("Muster");
   });
 });
