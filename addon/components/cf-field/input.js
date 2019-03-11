1f5992ef3bdc08d2cc8d1d72459d2658b28a9307
Christian Zosel <christian@zosel.ch>
2019-03-11 17:16:28 +0100

    Adapt meta field handling to latest caluma release
    Since https://github.com/projectcaluma/caluma/pull/318, the meta field
    returns an actual object instead of a JSONString.

--- addon/components/cf-field/input.js
+++ addon/components/cf-field/input.js
@@ -25,11 +25,10 @@ export default Component.extend({
   type: computed("field.question.__typename", function() {
     const typename = get(this, "field.question.__typename");
 
-    const meta = get(this, "field.question.meta") || "{}";
-    const customtype = JSON.parse(meta).widgetType;
+    const widgetType = get(this, "field.question.meta.widgetType");
 
     return (
-      customtype ||
+      widgetType ||
       (typename && mapping[typename]) ||
       typename.replace(/Question$/, "").toLowerCase()
     );
