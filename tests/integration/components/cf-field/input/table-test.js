77151b6b4823977be0042ea06c7c03bb17d01ce5
Christian Zosel <christian@zosel.ch>
2019-03-12 15:16:57 +0100

    WIP: testing

--- tests/integration/components/cf-field/input/table-test.js
+++ tests/integration/components/cf-field/input/table-test.js
@@ -1,12 +1,14 @@
 import { module, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
-import { render } from "@ember/test-helpers";
+import { render, click, settled } from "@ember/test-helpers";
 import hbs from "htmlbars-inline-precompile";
+import setupMirage from "ember-cli-mirage/test-support/setup-mirage";
 
 module("Integration | Component | cf-field/input/table", function(hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
 
-  test("it renders", async function(assert) {
+  hooks.beforeEach(function() {
     this.set("field", {
       id: "table-test",
       answer: {
@@ -26,22 +28,53 @@ module("Integration | Component | cf-field/input/table", function(hooks) {
         ]
       },
       question: {
+        __typename: "TableQuestion",
         rowForm: {
           questions: {
             edges: [
-              { node: { label: "First name" } },
-              { node: { label: "Last name" } }
+              {
+                node: {
+                  __typename: "TextQuestion",
+                  slug: "first-name",
+                  label: "First name"
+                }
+              },
+              {
+                node: {
+                  __typename: "TextQuestion",
+                  slug: "last-name",
+                  label: "Last name"
+                }
+              }
             ]
           }
         }
       }
     });
+  });
 
+  test("it renders", async function(assert) {
     await render(hbs`{{cf-field/input/table field=field}}`);
     assert.dom("th:nth-of-type(1)").hasText("First name");
     assert.dom("th:nth-of-type(2)").hasText("Last name");
 
     assert.dom("tbody > tr > td:nth-of-type(1)").hasText("Max");
     assert.dom("tbody > tr > td:nth-of-type(2)").hasText("Muster");
+  });
+
+  test("it can add rows", async function(assert) {
+    assert.expect(1);
+
+    await render(hbs`
+      {{cf-field/input
+        field=field
+      }}
+    `);
+
+    await click("button:nth-of-type(1)");
+
+    await settled();
+
+    assert.equal(this.get("showModal"), true);
   });
 });
