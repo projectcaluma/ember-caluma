da1c069f26dceb7d16bbadd0c8216389328c5c96
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2019-03-13 09:15:49 +0100

    ADD Accept-Language header with locale from ember-intl

--- addon/mixins/caluma-apollo-service-mixin.js
+++ addon/mixins/caluma-apollo-service-mixin.js
@@ -1,13 +1,17 @@
 import { computed } from "@ember/object";
 import Mixin from "@ember/object/mixin";
+import { inject as service } from "@ember/service";
 import {
   InMemoryCache,
   IntrospectionFragmentMatcher,
   defaultDataIdFromObject
 } from "apollo-cache-inmemory";
+import { setContext } from "apollo-link-context";
 import introspectionQueryResultData from "ember-caluma/-private/fragment-types";
 
 export default Mixin.create({
+  intl: service(),
+
   cache: computed(
     () =>
       new InMemoryCache({
@@ -22,5 +26,18 @@ export default Mixin.create({
           return defaultDataIdFromObject(obj);
         }
       })
-  )
+  ),
+
+  link: computed(function() {
+    let httpLink = this._super(...arguments);
+
+    let localeLink = setContext((_, { headers }) => ({
+      headers: {
+        ...headers,
+        "Accept-Language": this.get("intl.locale")
+      }
+    }));
+
+    return localeLink.concat(httpLink);
+  })
 });
