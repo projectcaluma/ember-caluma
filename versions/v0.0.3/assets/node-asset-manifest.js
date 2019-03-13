c02979b5ee53fa7166419912c70345e44783113e
Christian Zosel <christian@zosel.ch>
2019-03-13 15:07:27 +0100

    Deployed eeddd2a

---
+++ versions/v0.0.3/assets/node-asset-manifest.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+define('dummy/config/node-asset-manifest', function() {
+  return {
+    default: {"bundles":{"ember-caluma":{"assets":[{"uri":"/engines-dist/ember-caluma/config/environment-7955a085a8078c59d6e0b3e4ad4269b7.js","type":"js"}]}}}
+  };
+});
