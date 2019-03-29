46ebbe32cefaed4526bbae320de09caa67c073b2
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2019-03-29 10:56:18 +0100

    ADD initial proof of concept
    Not working: unreachable

--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -4,17 +4,22 @@ import { get } from "@ember/object";
 import { RouteQueryManager } from "ember-apollo-client";
 import gql from "graphql-tag";
 import { atob } from "ember-caluma/helpers/atob";
+import DummyComponent from "../components/dummy";
 
 export default Route.extend(RouteQueryManager, {
   apollo: service(),
 
   intl: service(),
-  calumaOptions: service(),
+  options: service(),
 
   init() {
     this._super(...arguments);
 
     this.intl.setLocale([...navigator.languages, "en-us"]);
+
+    this.options.set("overrides", {
+      "foo-test-1": DummyComponent
+    });
   },
 
   async model() {
