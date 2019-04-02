bc2d06f41386ad59435a2009d2f419f20cf140cf
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2019-04-02 12:36:26 +0200

    REMOVE obsolete component

--- tests/dummy/app/components/override.js
+++
@@ -1,7 +0,0 @@
-import Component from "@ember/component";
-import hbs from "htmlbars-inline-precompile";
-
-export default Component.extend({
-  layout: hbs`<div data-test-component-override />`,
-  tagName: ""
-});
