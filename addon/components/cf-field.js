17c173c7bcf7f1ed9615db361d36db7160b8cfab
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2019-04-02 12:53:22 +0200

    FIX computed property
    With the property name "override" Ember doesn't compute it.

--- addon/components/cf-field.js
+++ addon/components/cf-field.js
@@ -23,7 +23,7 @@ export default Component.extend({
   classNameBindings: ["field.question.hidden:uk-hidden"],
 
   options: service(),
-  override: computed("options._data.overrides.[]", function() {
+  componentOverride: computed("options._data.overrides.[]", function() {
     const override = this.field.question.meta.widgetOverride;
 
     if (override) {
