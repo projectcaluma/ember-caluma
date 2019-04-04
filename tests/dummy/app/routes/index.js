cd3a3bd2eefb638b2d5319c38c0b38ace492b5e7
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2019-04-04 09:33:27 +0200

    FIX dummy application
    * Removes namespace from calumaOptions.
    * Adds condition around dummy components.

--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -4,6 +4,7 @@ import { get } from "@ember/object";
 import { RouteQueryManager } from "ember-apollo-client";
 import gql from "graphql-tag";
 import { atob } from "ember-caluma/helpers/atob";
+import ENV from "ember-caluma/config/environment";
 
 export default Route.extend(RouteQueryManager, {
   apollo: service(),
@@ -16,22 +17,22 @@ export default Route.extend(RouteQueryManager, {
 
     this.intl.setLocale([...navigator.languages, "en-us"]);
 
-    this.calumaOptions.setNamespace("Foo Bar");
+    if (ENV.environment !== "production") {
+      this.calumaOptions.registerComponentOverride({
+        label: this.intl.t(
+          "caluma.form-builder.question.widgetOverrides.dummy-one"
+        ),
+        component: "dummy-one",
+        types: ["TextQuestion", "TextareaQuestion"]
+      });
 
-    this.calumaOptions.registerComponentOverride({
-      label: this.intl.t(
-        "caluma.form-builder.question.widgetOverrides.dummy-one"
-      ),
-      component: "dummy-one",
-      types: ["TextQuestion", "TextareaQuestion"]
-    });
-
-    this.calumaOptions.registerComponentOverride({
-      label: this.intl.t(
-        "caluma.form-builder.question.widgetOverrides.dummy-two"
-      ),
-      component: "dummy-two"
-    });
+      this.calumaOptions.registerComponentOverride({
+        label: this.intl.t(
+          "caluma.form-builder.question.widgetOverrides.dummy-two"
+        ),
+        component: "dummy-two"
+      });
+    }
   },
 
   async model() {
