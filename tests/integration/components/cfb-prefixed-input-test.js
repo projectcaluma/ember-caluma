700a8af85743c222187a2f62df3e622473553d7a
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2019-04-05 13:58:13 +0200

    UPDATE test for prefixed input component

--- tests/integration/components/cfb-prefixed-input-test.js
+++ tests/integration/components/cfb-prefixed-input-test.js
@@ -1,14 +1,24 @@
 import { module, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
-import { render } from "@ember/test-helpers";
+import { fillIn, render } from "@ember/test-helpers";
 import hbs from "htmlbars-inline-precompile";
 
 module("Integration | Component | cfb-prefixed-input", function(hooks) {
   setupRenderingTest(hooks);
 
   test("it renders", async function(assert) {
-    await render(hbs`{{cfb-prefixed-input}}`);
+    let slug = null;
+    this.set("update", value => {
+      slug = value;
+    });
 
-    assert.dom("[data-test-slug-input]").exists();
+    const selector = "[data-test-prefixed-input]";
+    const value = "test";
+
+    await render(hbs`{{cfb-prefixed-input update=update}}`);
+    assert.dom(selector).exists();
+
+    await fillIn(selector, value);
+    assert.equal(slug, value);
   });
 });
