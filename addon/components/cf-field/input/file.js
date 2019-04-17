4bc9606bc806ef96da00970690df8f75a4f25d92
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2019-04-17 10:26:27 +0200

    ADD file component with translations and stub tests

---
+++ addon/components/cf-field/input/file.js
@@ -0,0 +1,64 @@
+import Component from "@ember/component";
+import { inject as service } from "@ember/service";
+import { reads } from "@ember/object/computed";
+
+import getFileAnswerInfoQuery from "ember-caluma/gql/queries/get-fileanswer-info";
+import layout from "../../../templates/components/cf-field/input/file";
+import { computed } from "@ember/object";
+
+export default Component.extend({
+  layout,
+  tagName: "",
+
+  intl: service(),
+  apollo: service(),
+
+  downloadUrl: reads("field.answer.fileValue.downloadUrl"),
+
+  placeholder: computed("field.answer", function() {
+    return this.get("field.answer.fileValue")
+      ? this.intl.t("caluma.form.changeFile")
+      : this.intl.t("caluma.form.selectFile");
+  }),
+
+  actions: {
+    async download() {
+      const { downloadUrl } = await this.apollo.watchQuery(
+        {
+          query: getFileAnswerInfoQuery,
+          variables: {
+            id: this.field.answer.id
+          },
+          fetchPolicy: "cache-and-network"
+        },
+        "node.fileValue"
+      );
+
+      if (downloadUrl) {
+        window.open(downloadUrl, "_blank");
+      }
+    },
+
+    async save({ target }) {
+      const file = target.files[0];
+
+      if (!file) {
+        return;
+      }
+
+      const saved = await this.onSave(file.name);
+
+      const data = new FormData();
+      data.append("file", file);
+
+      const request = new XMLHttpRequest();
+      request.open("PUT", saved.fileValue.uploadUrl);
+      request.send(file);
+      request.onload = event => {
+        if (event.target.status == 200) {
+          target.parentNode.querySelector("[type=text]").value = "";
+        }
+      };
+    }
+  }
+});
---
+++ tests/integration/components/cf-field/input/file-test.js
@@ -0,0 +1,13 @@
+import { module, test } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { render } from "@ember/test-helpers";
+import hbs from "htmlbars-inline-precompile";
+
+module("Integration | Component | cf-field/input/file", function(hooks) {
+  setupRenderingTest(hooks);
+
+  test("it renders", async function(assert) {
+    await render(hbs`{{cf-field/input/file}}`);
+    assert.ok(this.element);
+  });
+});
