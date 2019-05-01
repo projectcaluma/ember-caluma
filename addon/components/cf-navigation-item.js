a50a44275e1021dc32b23ef131c4532017782cbb
Christian Zosel <christian@zosel.ch>
2019-05-01 15:31:30 +0200

    fix(cf-navigation): auto-link empty form sections to first subsection (#185)
    Also remove top-level question because it provided no real value and
    broke the styling

--- addon/components/cf-navigation-item.js
+++ addon/components/cf-navigation-item.js
@@ -1,10 +1,27 @@
 import Component from "@ember/component";
 import layout from "../templates/components/cf-navigation-item";
+import { computed } from "@ember/object";
 
 export default Component.extend({
   tagName: "",
   layout,
 
   section: null,
-  subSection: null
+  subSection: null,
+
+  _subSection: computed(
+    "subSection",
+    "field.childDocument.fields.[]",
+    function() {
+      if (
+        !this.get("subSection") &&
+        !(this.get("field.childDocument.fields") || []).some(
+          f => f.get("question.__typename") !== "FormQuestion"
+        )
+      ) {
+        return this.get("field.childDocument.fields.firstObject.question.slug");
+      }
+      return this.get("subSection");
+    }
+  )
 });
