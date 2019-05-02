798f00bd0c2648fb95083e594ca076bf581213fe
Christian Zosel <christian@zosel.ch>
2019-05-02 10:08:01 +0200

    fix(table): render download links for file questions in tables

--- addon/components/cf-field-value.js
+++ addon/components/cf-field-value.js
@@ -1,13 +1,17 @@
 import Component from "@ember/component";
 import layout from "../templates/components/cf-field-value";
 import { computed } from "@ember/object";
+import { inject as service } from "@ember/service";
+import getFileAnswerInfoQuery from "ember-caluma/gql/queries/get-fileanswer-info";
 
 export default Component.extend({
   layout,
 
+  apollo: service(),
+
   tagName: "span",
 
-  renderValue: computed("field.answer.value", function() {
+  value: computed("field.answer.value", function() {
     const field = this.get("field");
 
     switch (field.question.__typename) {
@@ -15,20 +19,45 @@ export default Component.extend({
         const option = field.question.choiceOptions.edges.find(
           edge => edge.node.slug === field.answer.value
         );
-        return option ? option.node.label : field.answer.value;
+        return { label: option ? option.node.label : field.answer.value };
       }
       case "MultipleChoiceQuestion": {
         const answerValue = field.answer.value || [];
         const options = field.question.multipleChoiceOptions.edges.filter(
           edge => answerValue.includes(edge.node.slug)
         );
-        return options && options.length
-          ? options.map(edge => edge.node.label).join(", ")
-          : answerValue.join(", ");
+        return {
+          label:
+            options && options.length
+              ? options.map(edge => edge.node.label).join(", ")
+              : answerValue.join(", ")
+        };
+      }
+      case "FileQuestion": {
+        const answerValue = field.answer.value;
+        return {
+          fileAnswerId: answerValue && field.answer.id,
+          label: answerValue && answerValue.name
+        };
       }
 
       default:
-        return field.answer.value;
+        return {
+          label: field.answer.value
+        };
     }
-  })
+  }),
+  actions: {
+    async download(fileAnswerId) {
+      const { downloadUrl } = await this.apollo.watchQuery(
+        {
+          query: getFileAnswerInfoQuery,
+          variables: { id: fileAnswerId },
+          fetchPolicy: "cache-and-network"
+        },
+        "node.fileValue"
+      );
+      window.open(downloadUrl, "_blank");
+    }
+  }
 });
