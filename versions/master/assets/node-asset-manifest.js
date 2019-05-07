aecca6774932f3730caf0dad3f1fac2fedcb4266
Travis CI User <travis@example.org>
2019-05-07 11:35:05 +0000

    Deployed 7cc5eef

--- versions/master/assets/node-asset-manifest.js
+++ versions/master/assets/node-asset-manifest.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 define('dummy/config/node-asset-manifest', function() {
   return {
-    default: {"bundles":{"ember-caluma":{"assets":[{"uri":"/engines-dist/ember-caluma/config/environment-7955a085a8078c59d6e0b3e4ad4269b7.js","type":"js"}]}}}
+    default: {"bundles":{"ember-caluma":{"assets":[{"uri":"/engines-dist/ember-caluma/config/environment-7f169f7bce1bb75ea9d626ae6be08da9.js","type":"js"}]}}}
   };
 });
