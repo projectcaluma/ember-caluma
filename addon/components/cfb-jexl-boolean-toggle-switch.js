46f0d308b634cd57cd93c2abffa253395068467d
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2019-05-13 14:47:29 +0200

    feat: add jexl textarea for isRequired

--- addon/components/cfb-jexl-boolean-toggle-switch.js
+++ addon/components/cfb-jexl-boolean-toggle-switch.js
@@ -3,6 +3,7 @@ import layout from "../templates/components/cfb-jexl-boolean-toggle-switch";
 import jexl from "jexl";
 import { reads } from "@ember/object/computed";
 import { task } from "ember-concurrency";
+import { computed } from "@ember/object";
 
 export default RenderComponent.extend({
   layout,
@@ -17,6 +18,11 @@ export default RenderComponent.extend({
 
   _boolValue: task(function*() {
     return yield jexl.eval(this.get("value"));
+  }),
+
+  disabled: computed("value", function() {
+    const value = this.get("value");
+    return !(value === "true" || value === "false");
   }),
 
   actions: {
