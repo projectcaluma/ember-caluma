581de158a5830e705596b3bc6d68de0b6eefa308
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-05-21 08:25:04 +0200

    feat(form): enable widget overrides for forms

---
+++ addon/helpers/get-widget.js
@@ -0,0 +1,45 @@
+import Helper from "@ember/component/helper";
+import { inject as service } from "@ember/service";
+import { warn } from "@ember/debug";
+
+/**
+ * Helper for getting the right widget for a field.
+ *
+ * This helper expects a field as first positional parameter. It checks if the
+ * field has a widget override in it's metadata. If one exists it checks if
+ * said widget was registered in the caluma options service and then returns
+ * the widget name.
+ *
+ * ```hbs
+ * {{component (get-widget field default="cf-form") foo=bar}}
+ * ```
+ *
+ * @function getWidget
+ * @param {Array} params
+ * @param {Object} [options]
+ * @param {String} [options.default]
+ */
+export default Helper.extend({
+  calumaOptions: service(),
+
+  compute([field], { default: defaultWidget = "cf-field/input" }) {
+    try {
+      const widget = field.question.meta.widgetOverride;
+      const overrides = this.calumaOptions.getComponentOverrides();
+      const override = overrides.find(({ component }) => component === widget);
+
+      if (!override) {
+        warn(
+          `Widget override "${widget}" is not registered. Please register it by calling \`calumaOptions.registerComponentOverride\``,
+          { id: "ember-caluma.unregistered-override" }
+        );
+
+        throw new Error();
+      }
+
+      return widget;
+    } catch (e) {
+      return defaultWidget;
+    }
+  }
+});
---
+++ tests/integration/helpers/get-widget-test.js
@@ -0,0 +1,47 @@
+import { module, test } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { render } from "@ember/test-helpers";
+import hbs from "htmlbars-inline-precompile";
+
+module("Integration | Helper | get-widget", function(hooks) {
+  setupRenderingTest(hooks);
+
+  test("it returns valid overrides", async function(assert) {
+    const calumaOptions = this.owner.lookup("service:calumaOptions");
+
+    calumaOptions.registerComponentOverride({
+      label: "Some Component",
+      component: "some-component"
+    });
+
+    await render(
+      hbs`{{get-widget (hash question=(hash meta=(hash widgetOverride="some-component")))}}`
+    );
+
+    assert.dom(this.element).hasText("some-component");
+  });
+
+  test("it doesn't return an invalid override", async function(assert) {
+    await render(
+      hbs`{{get-widget (hash question=(hash meta=(hash widgetOverride="some-component")))}}`
+    );
+
+    assert.dom(this.element).hasText("cf-field/input");
+  });
+
+  test("it has a fallback", async function(assert) {
+    await render(hbs`{{get-widget null}}`);
+
+    assert.dom(this.element).hasText("cf-field/input");
+
+    await render(hbs`{{get-widget undefined}}`);
+
+    assert.dom(this.element).hasText("cf-field/input");
+  });
+
+  test("it can pass the default widget", async function(assert) {
+    await render(hbs`{{get-widget null default="cf-form"}}`);
+
+    assert.dom(this.element).hasText("cf-form");
+  });
+});
