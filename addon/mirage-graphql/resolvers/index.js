96f3bef43158aa98455210d342ee1dac9b07fb8e
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-05-22 09:48:59 +0200

    chore(mirage): fix comment for generic scalar resolver

--- addon/mirage-graphql/resolvers/index.js
+++ addon/mirage-graphql/resolvers/index.js
@@ -3,7 +3,7 @@ import { GraphQLDateTime } from "graphql-iso-date";
 export default {
   DateTime: GraphQLDateTime,
   // generic scalar serializes from a string to an object but deserializes as
-  // object for some reason ¯\_(ツ)_/¯. But hey, it works!
+  // object because of backend limitations.
   GenericScalar: {
     serialize(str) {
       return typeof str === "string" ? JSON.parse(str) : str;
