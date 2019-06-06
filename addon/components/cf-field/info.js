bab516685ffdd20edf5a1c13b351bd95731523d4
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-06-06 10:17:31 +0200

    fix(form): allow stacking of info modals

--- addon/components/cf-field/info.js
+++ addon/components/cf-field/info.js
@@ -3,6 +3,5 @@ import layout from "../../templates/components/cf-field/info";
 
 export default Component.extend({
   layout,
-  classNames:
-    "uk-padding-small uk-padding-remove-vertical uk-padding-remove-right uk-flex uk-flex-middle"
+  classNames: ["uk-margin-small-left", "uk-flex", "uk-flex-middle"]
 });
