ca1293b7681b50b54fa0468a599138262e77ba0b
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-06-12 09:13:40 +0200

    feat(form): prevent displaying of empty forms

--- addon/components/cf-navigation-item.js
+++ addon/components/cf-navigation-item.js
@@ -1,31 +1,7 @@
 import Component from "@ember/component";
 import layout from "../templates/components/cf-navigation-item";
-import { computed } from "@ember/object";
 
 export default Component.extend({
   tagName: "",
-  layout,
-
-  section: null,
-  subSection: null,
-
-  _subSection: computed(
-    "subSection",
-    "field.childDocument.fields.@each.hidden",
-    function() {
-      if (
-        !this.get("subSection") &&
-        !this.getWithDefault("field.childDocument.fields", []).some(
-          f => f.get("question.__typename") !== "FormQuestion"
-        )
-      ) {
-        const target = this.getWithDefault(
-          "field.childDocument.fields",
-          []
-        ).find(field => field.hidden === false);
-        return target && target.question.slug;
-      }
-      return this.get("subSection");
-    }
-  )
+  layout
 });
