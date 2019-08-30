76888cf2b05c8cae0652ab57cdd7555b124286f3
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2019-08-30 14:07:18 +0200

    feat(options): hide archived options

--- addon-mirage-support/factories/option.js
+++ addon-mirage-support/factories/option.js
@@ -3,5 +3,6 @@ import { Factory } from "ember-cli-mirage";
 export default Factory.extend({
   slug: i => `option-${i + 1}`,
   label: i => `Option ${i + 1}`,
-  meta: () => ({})
+  meta: () => ({}),
+  isArchived: false
 });
