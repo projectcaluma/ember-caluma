f220ea1d211e867076ac94deb139493cfcfa38b8
Travis CI User <travis@example.org>
2019-12-02 10:47:14 +0000

    Deployed 4016c27

---
+++ versions/v4.0.2/assets/node-asset-manifest.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+define('dummy/config/node-asset-manifest', function() {
+  return {
+    default: {"bundles":{"ember-caluma":{"assets":[{"uri":"/engines-dist/ember-caluma/config/environment-3b977884c507d5eb756ee684a4f04e54.js","type":"js"}]}}}
+  };
+});
