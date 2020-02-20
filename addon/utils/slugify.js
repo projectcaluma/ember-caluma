00b96548fffe1a202f8a043b9625ae81b72b8662
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2020-02-20 12:10:40 +0100

    feat(utils): increase slugify max-length to 150

--- addon/utils/slugify.js
+++ addon/utils/slugify.js
@@ -3,5 +3,5 @@ import slugify from "slugify";
 export default function(value) {
   return slugify(value.toLowerCase(), {
     remove: /((?![a-z0-9-\s]).)/g
-  }).substr(0, 50);
+  }).substr(0, 150);
 }
