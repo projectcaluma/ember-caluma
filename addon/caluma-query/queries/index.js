04b380c2e818191c89111cc0bedc9450bb90c9d9
Jonas Cosandey <jonas.cosandey@adfinis.com>
2020-09-24 15:35:51 +0200

    feat(query): add a case query

--- addon/caluma-query/queries/index.js
+++ addon/caluma-query/queries/index.js
@@ -1,7 +1,12 @@
 import WorkItemQuery from "./work-item";
+import CaseQuery from "./case";
 
 export function allWorkItems(options) {
   return new WorkItemQuery(options);
 }
 
-export default { allWorkItems };
+export function allCases(options) {
+  return new CaseQuery(options);
+}
+
+export default { allWorkItems, allCases };
--- addon/initializers/caluma-query-models.js
+++ addon/initializers/caluma-query-models.js
@@ -1,7 +1,9 @@
 import WorkItemModel from "ember-caluma/caluma-query/models/work-item";
+import CaseModel from "ember-caluma/caluma-query/models/case";
 
 export function initialize(application) {
   application.register("caluma-query-model:work-item", WorkItemModel);
+  application.register("caluma-query-model:case", CaseModel);
 }
 
 export default {
