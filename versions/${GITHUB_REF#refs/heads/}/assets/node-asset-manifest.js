0bed6e9e2f77ad1411fb528aafcc7c2943718e91
Tomster <tomster@emberjs.com>
2020-11-11 12:35:01 +0000

    Deployed 2a819c0

--- versions/${GITHUB_REF#refs/heads/}/assets/node-asset-manifest.js
+++ versions/${GITHUB_REF#refs/heads/}/assets/node-asset-manifest.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 define('dummy/config/node-asset-manifest', function() {
   return {
-    default: {"bundles":{"ember-caluma":{"assets":[{"uri":"/engines-dist/ember-caluma/config/environment-cd644d1ac6fc5e36f5bd1d61e8107fce.js","type":"js"}]}}}
+    default: {"bundles":{"ember-caluma":{"assets":[{"uri":"/engines-dist/ember-caluma/config/environment-d43bfda17a6a10b268499c0eb7ef7fd2.js","type":"js"}]}}}
   };
 });
