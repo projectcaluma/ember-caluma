fc3f6031aac882827a718c0b8ea6efefb3f409d2
Tomster <tomster@emberjs.com>
2020-11-24 09:01:09 +0000

    Deployed 2d875d6

---
+++ engines-dist/ember-caluma/config/environment-cd644d1ac6fc5e36f5bd1d61e8107fce.js
@@ -0,0 +1,1 @@
+define("ember-caluma/config/environment",(function(){return{default:{modulePrefix:"ember-caluma","ember-validated-form":{theme:"uikit",defaults:{label:"cfb-label"}},historySupportMiddleware:!0}}}))
