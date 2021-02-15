3b6de3688f78d3ddfa352e1f7ef339e9c326b118
Tomster <tomster@emberjs.com>
2021-02-15 07:31:45 +0000

    Deployed 5233af4

--- ember-caluma/versions/v0.0.4/assets/node-asset-manifest.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-define('dummy/config/node-asset-manifest', function() {
-  return {
-    default: {"bundles":{"ember-caluma":{"assets":[{"uri":"/engines-dist/ember-caluma/config/environment-7955a085a8078c59d6e0b3e4ad4269b7.js","type":"js"}]}}}
-  };
-});
--- ember-caluma/versions/v2.5.0/assets/node-asset-manifest.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-define('dummy/config/node-asset-manifest', function() {
-  return {
-    default: {"bundles":{"ember-caluma":{"assets":[{"uri":"/engines-dist/ember-caluma/config/environment-7f169f7bce1bb75ea9d626ae6be08da9.js","type":"js"}]}}}
-  };
-});
