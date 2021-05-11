71cb7a290f1152d6f178d2d29e2fe91bfe038f11
yelinz <yelin.zhang@adfinis.com>
2021-05-11 15:01:02 +0200

    fix(work-item-button): add docs and improve component

--- addon/components/work-item-button.js
+++ addon/components/work-item-button.js
@@ -1,29 +1,44 @@
 import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
 import { queryManager } from "ember-apollo-client";
-import { restartableTask } from "ember-concurrency-decorators";
+import { dropTask } from "ember-concurrency-decorators";
 
 import cancelWorkItem from "ember-caluma/gql/mutations/cancel-work-item.graphql";
 import completeWorkItem from "ember-caluma/gql/mutations/complete-work-item.graphql";
 import skipWorkItem from "ember-caluma/gql/mutations/skip-work-item.graphql";
 
-export default class ComponentsWorkItemButtonComponent extends Component {
+/**
+ * Component to render a UkButton which mutates the given work item.
+ *
+ * ```hbs
+ * <WorkItemButton @mutation="complete" @workItemId=""/>
+ * ```
+ *
+ * @class WorkItemButtonComponent
+ */
+export default class WorkItemButtonComponent extends Component {
+  /**
+   * The mutation which is going to be used for the work item.
+   * Valid mutations are: complete, skip, cancel
+   * @argument {String} mutation
+   */
+  /**
+   * The id of the work item
+   * @argument {String} workItemId
+   */
+
   @queryManager apollo;
 
   @service intl;
 
-  constructor(owner, args) {
-    super(owner, args);
+  cancelWorkItemMutation = cancelWorkItem;
+  completeWorkItemMutation = completeWorkItem;
+  skipWorkItemMutation = skipWorkItem;
 
-    this.cancelWorkItem = cancelWorkItem;
-    this.completeWorkItem = completeWorkItem;
-    this.skipWorkItem = skipWorkItem;
-  }
-
-  @restartableTask
+  @dropTask
   *mutate() {
     yield this.apollo.mutate({
-      mutation: this[`${this.args.mutation}WorkItem`],
+      mutation: this[`${this.args.mutation}WorkItemMutation`],
       variables: { id: this.args.workItemId },
     });
   }
