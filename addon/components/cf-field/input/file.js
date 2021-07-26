projectcaluma/ember-caluma

commit 16c5d8b49aae20587d7797d7e2c46b7a8c3a9f9e

refactor(cf): use fetch API for file uploads
Replace XMLHttpRequest implementation for file
uploads with fetch API.

addon/components/cf-field/input/file.js
@@ -39,20 +39,14 @@ export default Component.extend({
       const data = new FormData();
       data.append("file", file);
 
-      const request = new XMLHttpRequest();
-
-      request.addEventListener("error", (event) => reject(event));
-      request.addEventListener("abort", (event) => reject(event));
-      request.addEventListener("load", (event) => {
-        if (event.target.status === 200) {
-          resolve(event);
-        } else {
-          reject(event);
-        }
-      });
-
-      request.open("PUT", url);
-      request.send(file);
+      fetch(url, {
+        method: "PUT",
+        body: data,
+      })
+        .then((response) =>
+          response.ok ? resolve(response) : reject(response)
+        )
+        .catch((error) => reject(error));
     });
   },
 
@@ -88,7 +82,7 @@ export default Component.extend({
           name: file.name,
           downloadUrl: fileValue.downloadUrl,
         });
-      } catch (event) {
+      } catch (error) {
         await this.onSave(null);
         this.set("field._errors", [{ type: "uploadFailed" }]);
       } finally {

tests/integration/components/cf-field/input/file-test.js
@@ -1,4 +1,4 @@
-import { render, triggerEvent } from "@ember/test-helpers";
+import { render, triggerEvent, waitUntil } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
 import { setupMirage } from "ember-cli-mirage/test-support";
 import { setupIntl } from "ember-intl/test-support";
@@ -17,7 +17,7 @@ module("Integration | Component | cf-field/input/file", function (hooks) {
   });
 
   test("it allows to upload a file", async function (assert) {
-    assert.expect(0);
+    assert.expect(6);
 
     this.set("field", {
       answer: {
@@ -27,7 +27,7 @@ module("Integration | Component | cf-field/input/file", function (hooks) {
     });
 
     this.set("onSave", (name) => ({
-      value: { uploadUrl: `/minio/upload/${name}` },
+      fileValue: { uploadUrl: `/minio/upload/${name}` },
     }));
 
     const payload_good = new File(["test"], "good.txt", { type: "text/plain" });
@@ -36,8 +36,29 @@ module("Integration | Component | cf-field/input/file", function (hooks) {
     await render(hbs`{{cf-field/input/file field=field onSave=onSave}}`);
 
     await triggerEvent("input[type=file]", "change", { files: [] });
+    assert.equal(this.field.answer.value.name, undefined);
+    assert.equal(this.field._errors, undefined);
+
     await triggerEvent("input[type=file]", "change", { files: [payload_fail] });
+    await waitUntil(() => this.field._errors[0].type === "uploadFailed", {
+      timeout: 2000,
+    });
+    assert.equal(this.field.answer.value.name, undefined);
+    assert.equal(this.field._errors[0].type, "uploadFailed");
+
+    this.set("field", {
+      answer: {
+        id: btoa("FileAnswer:1"),
+        value: {},
+      },
+    });
+
     await triggerEvent("input[type=file]", "change", { files: [payload_good] });
+    await waitUntil(() => this.field.answer.value.name === "good.txt", {
+      timeout: 2000,
+    });
+    assert.equal(this.field.answer.value.name, "good.txt");
+    assert.equal(this.field._errors, undefined);
   });
 
   test("it allows to download a file", async function (assert) {

commit 56442facd51fc1cce0d9c21e1bd8fffd6acec34e

fix(file upload): don't use formdata for minio upload
It seems that minio expects the request body to be the plain file
instead of a FormData object.

This fixes a regression introduced in #1287.

addon/components/cf-field/input/file.js
@@ -53,12 +53,9 @@ export default class CfFieldInputFileComponent extends Component {
     const { fileValue } = await this.args.onSave(file.name);
 
     try {
-      const data = new FormData();
-      data.append("file", file);
-
       const response = await fetch(fileValue.uploadUrl, {
         method: "PUT",
-        body: data,
+        body: file,
       });
 
       if (!response.ok) {