513e58ad093a601ce0f9a43b67959aeace082acb
Stephan Hug <stephan.hug@adfinis.com>
2021-08-05 10:13:53 +0200

    fix(demo-app): added required "validation" route to router and added corresponding translations.

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -15,6 +15,7 @@ Router.map(function () {
     this.route("queries");
     this.route("migration");
     this.route("buttons");
+    this.route("validation");
   });
 
   this.route("demo", function () {
