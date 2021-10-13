ef84f041dc11a75e31aeb7a9fa383292c49521a8
Jonas Metzener <jonas.metzener@adfinis.com>
2021-10-13 18:18:10 +0200

    fix(core): move caluma query model blueprints to core

---
+++ packages/core/blueprints/caluma-query-model/files/app/caluma-query/models/__name__.js
@@ -0,0 +1,3 @@
+import <%= classifiedModuleName %>Model from "@projectcaluma/ember-core/caluma-query/models/<%= dasherizedModuleName %>";
+
+export default class Custom<%= classifiedModuleName %>Model extends <%= classifiedModuleName %>Model {}
