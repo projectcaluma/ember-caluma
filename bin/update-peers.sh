c1f934c2652d694b6b3c1a3459b94326645cc156
Jonas Metzener <jonas.metzener@adfinis.com>
2021-11-16 17:46:20 +0100

    chore(release): add script to update cross dependencies after release

--- bin/update-peers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for pkg in ../**/package.json
-do
-  name=$(node -p "require('$pkg').name")
-  version=$(node -p "require('$pkg').version")
-
-  sed -i "s#\"$name\": \".*\"#\"$name\": \"$version\"#g" package.json
-done
