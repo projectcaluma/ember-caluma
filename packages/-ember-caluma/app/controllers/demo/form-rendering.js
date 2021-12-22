a1a50dbdcb9bb95d4dd0814a2008b4b020a4b2c3
Jonas Metzener <jonas.metzener@adfinis.com>
2021-12-22 16:30:09 +0100

    chore(docs): support subforms in form rendering demo

--- packages/-ember-caluma/app/controllers/demo/form-rendering.js
+++ packages/-ember-caluma/app/controllers/demo/form-rendering.js
@@ -5,6 +5,8 @@ import { inject as service } from "@ember/service";
 export default class DemoFormRenderingController extends Controller {
   @service notification;
 
+  queryParams = ["displayedForm"];
+
   @action actionButtonOnSuccess() {
     this.notification.success("Successfully submitted the form!");
   }
