242007a9b5010fc99824b7f03d8102095904403f
Jonas Lehmann <jonas.lehmann@adfinis.com>
2022-01-10 09:32:36 +0100

    fix(form): fix dependencies for ember-autoresize-modifier

--- packages/form/blueprints/@projectcaluma/ember-form/index.js
+++ packages/form/blueprints/@projectcaluma/ember-form/index.js
@@ -12,6 +12,7 @@ module.exports = {
         { name: "ember-math-helpers" },
         { name: "ember-pikaday" },
         { name: "ember-power-select" },
+        { name: "ember-autoresize-modifier" },
       ],
     });
   },
