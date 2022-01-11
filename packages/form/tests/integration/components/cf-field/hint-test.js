d74e2014ae19c51b568d76d1d43c66c0bb37b3bc
Alana Luyten <alana.luyten@adfinis.com>
2022-01-11 16:30:47 +0100

    feat(cf-field): add optional hints for form fields
    BREAKING CHANGE: Question hints requires Caluma >= v7.15.0
    
    Add option to create hints for certain question types. These
    are displayed below the input field and can be used to provide
    short, informative messages. Hints are available for all question
    types except for form, static and action button questions.

---
+++ packages/form/tests/integration/components/cf-field/hint-test.js
@@ -0,0 +1,49 @@
+import { render } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
+import { setupIntl } from "ember-intl/test-support";
+import { setupRenderingTest } from "ember-qunit";
+import { module, test } from "qunit";
+
+module("Integration | Component | cf-field/hint", function (hooks) {
+  setupRenderingTest(hooks);
+  setupIntl(hooks);
+
+  hooks.beforeEach(function () {
+    const form = {
+      slug: "some-form",
+      __typename: "Form",
+      questions: [
+        {
+          slug: "question-1",
+          label: "Test",
+          isRequired: "true",
+          isHidden: "true",
+          hintText: "Test",
+          __typename: "TextQuestion",
+        },
+      ],
+    };
+
+    const raw = {
+      id: 1,
+      __typename: "Document",
+      answers: [],
+      rootForm: form,
+      forms: [form],
+    };
+
+    const document = new (this.owner.factoryFor("caluma-model:document").class)(
+      { raw, owner: this.owner }
+    );
+    this.field = document.fields[0];
+  });
+
+  test("it renders", async function (assert) {
+    assert.expect(2);
+
+    await render(hbs`<CfField::hint @field={{field}} />`);
+
+    assert.dom(`div[data-test-field-hint='${this.field.pk}']`).exists();
+    assert.dom(`div[data-test-field-hint='${this.field.pk}']`).hasText("Test");
+  });
+});
