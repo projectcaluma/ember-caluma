0a048194a956311a5c553535fa1522e8bd073897
Jonas Metzener <jonas.metzener@adfinis.com>
2022-01-25 22:02:23 +0100

    fix(form): fix closing of info modal

--- packages/form/addon/components/cf-field/info.js
+++ packages/form/addon/components/cf-field/info.js
@@ -1,21 +1,6 @@
-import { action } from "@ember/object";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 
 export default class CfFieldInfoComponent extends Component {
   @tracked modalVisible = false;
-
-  @action
-  showModal(e) {
-    e.preventDefault();
-
-    this.modalVisible = true;
-  }
-
-  @action
-  hideModal(e) {
-    e.preventDefault();
-
-    this.modalVisible = false;
-  }
 }
