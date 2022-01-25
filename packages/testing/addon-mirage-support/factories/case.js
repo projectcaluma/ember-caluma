3642d6c85ef6d7bcca310069566d909aff166051
Jonas Metzener <jonas.metzener@adfinis.com>
2022-01-25 22:01:39 +0100

    chore(faker): fix faker deprecations

--- packages/testing/addon-mirage-support/factories/case.js
+++ packages/testing/addon-mirage-support/factories/case.js
@@ -1,4 +1,4 @@
-import faker from "@faker-js/faker";
+import { faker } from "@faker-js/faker";
 import { Factory } from "miragejs";
 
 const STATUS = ["RUNNING", "COMPLETED", "CANCELED", "SUSPENDED"];
