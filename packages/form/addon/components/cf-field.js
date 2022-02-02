8d8587a329042d699c6f27ab4649b0d646685eac
Jonas Metzener <jonas.metzener@adfinis.com>
2022-02-02 10:08:53 +0100

    fix(action-button): hide field wrapper if action button is not displayed

--- packages/form/addon/components/cf-field.js
+++ packages/form/addon/components/cf-field.js
@@ -31,7 +31,11 @@ export default class CfFieldComponent extends Component {
   get visible() {
     return (
       !this.args.field?.hidden &&
-      !hasQuestionType(this.args.field?.question, "form")
+      !hasQuestionType(this.args.field?.question, "form") &&
+      !(
+        hasQuestionType(this.args.field?.question, "action-button") &&
+        this.args.disabled
+      )
     );
   }
 
