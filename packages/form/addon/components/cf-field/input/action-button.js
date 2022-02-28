22edb00e7b82af13151b90d78b136f6f10c9834c
Jonas Metzener <jonas.metzener@adfinis.com>
2022-02-28 08:07:00 +0100

    chore(action-button): exclude action button from build if workflow is not installed

--- packages/form/addon/components/cf-field/input/action-button.js
+++ packages/form/addon/components/cf-field/input/action-button.js
@@ -1,61 +1,78 @@
 import { assert } from "@ember/debug";
 import { action } from "@ember/object";
+import { dependencySatisfies, macroCondition } from "@embroider/macros";
 import Component from "@glimmer/component";
 import { confirm } from "ember-uikit";
 
-export default class CfFieldInputActionButtonComponent extends Component {
-  constructor(...args) {
-    super(...args);
+let CfFieldInputActionButtonComponent;
 
-    assert(
-      "The document must have a `workItemUuid` for `<CfField::Input::ActionButton />` to work.",
-      this.args.field.document.workItemUuid
-    );
-  }
+if (macroCondition(dependencySatisfies("@projectcaluma/ember-workflow", ""))) {
+  CfFieldInputActionButtonComponent = class extends Component {
+    constructor(...args) {
+      super(...args);
 
-  get workItem() {
-    return (
-      this.args.context?.actionButtonWorkItemId ||
-      this.args.field.document.workItemUuid
-    );
-  }
-
-  get action() {
-    return this.args.field.question.raw.action.toLowerCase();
-  }
-
-  get color() {
-    return this.args.field.question.raw.color.toLowerCase();
-  }
-
-  get validateOnEnter() {
-    return (
-      this.args.field.question.raw.action === "COMPLETE" &&
-      this.args.field.question.raw.validateOnEnter
-    );
-  }
-
-  @action
-  async beforeMutate(validateFn) {
-    if (
-      this.args.field.question.raw.action === "COMPLETE" &&
-      !(await validateFn())
-    ) {
-      return false;
+      assert(
+        "The document must have a `workItemUuid` for `<CfField::Input::ActionButton />` to work.",
+        this.args.field.document.workItemUuid
+      );
     }
 
-    const confirmText = this.args.field.question.raw.infoText;
+    get workItem() {
+      return (
+        this.args.context?.actionButtonWorkItemId ||
+        this.args.field.document.workItemUuid
+      );
+    }
 
-    return !confirmText || confirm(confirmText);
-  }
+    get action() {
+      return this.args.field.question.raw.action.toLowerCase();
+    }
 
-  @action
-  onSuccess() {
-    return this.args.context?.actionButtonOnSuccess?.();
-  }
+    get color() {
+      return this.args.field.question.raw.color.toLowerCase();
+    }
 
-  @action
-  onError(error) {
-    return this.args.context?.actionButtonOnError?.(error);
-  }
+    get validateOnEnter() {
+      return (
+        this.args.field.question.raw.action === "COMPLETE" &&
+        this.args.field.question.raw.validateOnEnter
+      );
+    }
+
+    @action
+    async beforeMutate(validateFn) {
+      if (
+        this.args.field.question.raw.action === "COMPLETE" &&
+        !(await validateFn())
+      ) {
+        return false;
+      }
+
+      const confirmText = this.args.field.question.raw.infoText;
+
+      return !confirmText || confirm(confirmText);
+    }
+
+    @action
+    onSuccess() {
+      return this.args.context?.actionButtonOnSuccess?.();
+    }
+
+    @action
+    onError(error) {
+      return this.args.context?.actionButtonOnError?.(error);
+    }
+  };
+} else {
+  CfFieldInputActionButtonComponent = class extends Component {
+    constructor(...args) {
+      super(...args);
+
+      assert(
+        "@projectcaluma/ember-workflow must be installed to enable the usage of the action button questions"
+      );
+    }
+  };
 }
+
+export default CfFieldInputActionButtonComponent;
