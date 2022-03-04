c3132c9a73ec59030fd1ab7611b3a2eb464feacd
Jonas Metzener <jonas.metzener@adfinis.com>
2022-03-04 23:28:03 +0100

    feat(form-builder): add field for editing meta

--- packages/form-builder/addon/components/cfb-code-editor.js
+++ packages/form-builder/addon/components/cfb-code-editor.js
@@ -1,25 +1,70 @@
 import { action } from "@ember/object";
+import { addObserver } from "@ember/object/observers";
 import Component from "@glimmer/component";
-import { tracked } from "@glimmer/tracking";
 import { CodeJar } from "codejar";
 import Prism from "prismjs";
+
+import "prismjs/components/prism-json.js";
 import "prismjs/components/prism-jexl.js";
 import "prismjs/components/prism-markdown.js";
 
 export default class CfbCodeEditorComponent extends Component {
-  editor;
-  @tracked show = false;
+  _editor = null;
+  _cursor = null;
+  _lastValue = null;
+
+  get value() {
+    const value = this.args.value;
+
+    if (this.args.language === "json" && typeof value === "object") {
+      return JSON.stringify(value?.unwrap?.() ?? value, null, 2);
+    }
+
+    return this.args.value;
+  }
+
+  @action
+  onUpdate(value) {
+    if (this._lastValue === value) return;
+
+    this._cursor = this._editor.save();
+
+    if (this.args.language === "json") {
+      try {
+        value = JSON.parse(value);
+      } catch {
+        // update value directly
+      }
+    }
+
+    this._lastValue = value;
+    this.args.update(value);
+    this.args.setDirty();
+  }
+
+  @action
+  updateCode() {
+    this._editor.updateCode(this.value);
+
+    if (this._cursor) {
+      this._editor.restore(this._cursor);
+      this._cursor = null;
+    }
+  }
 
   @action
   didInsertNode(element) {
-    const highlight = (editor) => Prism.highlightElement(editor);
-    this.editor = CodeJar(element, highlight);
-    this.editor.updateCode(this.args.value);
-    this.editor.onUpdate(this.args.update);
+    this._editor = CodeJar(element, (editor) => Prism.highlightElement(editor));
+    this._editor.onUpdate(this.onUpdate);
+
+    this.updateCode();
+
+    // eslint-disable-next-line ember/no-observers
+    addObserver(this.args, "value", this, "updateCode");
   }
 
   @action
   willDestroyNode() {
-    this.editor.destroy();
+    this._editor.destroy();
   }
 }
