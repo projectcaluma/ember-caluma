c34cda691f78623f41da1f9d75a39efce7e0ff0f
Jonas Metzener <jonas.metzener@adfinis.com>
2022-03-25 10:41:45 +0100

    feat(form): show technical details of backend errors in UI

---
+++ packages/form/addon/helpers/format-graphql-error.js
@@ -0,0 +1,21 @@
+import { helper } from "@ember/component/helper";
+
+export function formatGraphqlErrorObject(error) {
+  try {
+    const path = error.path.join(".");
+    const { line, column } = error.locations.at(-1);
+
+    return `${path}:${line}:${column}: ${error.message}`;
+  } catch (e) {
+    return null;
+  }
+}
+
+export function formatGraphqlError(error) {
+  return (
+    error?.errors?.map(formatGraphqlErrorObject).filter(Boolean).join("\n") ??
+    ""
+  );
+}
+
+export default helper(([error]) => formatGraphqlError(error));
---
+++ packages/form/app/helpers/format-graphql-error.js
@@ -0,0 +1,1 @@
+export { default } from "@projectcaluma/ember-form/helpers/format-graphql-error";
