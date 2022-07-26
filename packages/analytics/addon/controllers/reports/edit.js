ba9162c5a7db2e41b21adac569ed3ae1c16753ee
Falk <falk.neumann@adfinis.com>
2022-07-26 15:45:37 +0200

    fix(analytics): move deleteTable task to edit controller

--- packages/analytics/addon/controllers/reports/edit.js
+++ packages/analytics/addon/controllers/reports/edit.js
@@ -2,9 +2,10 @@ import Controller from "@ember/controller";
 import { inject as service } from "@ember/service";
 import { tracked } from "@glimmer/tracking";
 import { queryManager } from "ember-apollo-client";
-import { task } from "ember-concurrency";
+import { dropTask, task } from "ember-concurrency";
 import { trackedTask } from "ember-resources/util/ember-concurrency";
 
+import removeAnalyticsTableMutation from "@projectcaluma/ember-analytics/gql/mutations/remove-analytics-table.graphql";
 import getAnalyticsTable from "@projectcaluma/ember-analytics/tasks/get-analytics-table";
 
 export default class ReportsEditController extends Controller {
@@ -18,5 +19,26 @@ export default class ReportsEditController extends Controller {
 
   get currentRoute() {
     return this.router.currentRouteName.split(".").pop();
+  }
+
+  @dropTask
+  *deleteTable() {
+    try {
+      yield this.apollo.mutate({
+        mutation: removeAnalyticsTableMutation,
+        fetchPolicy: "network-only",
+        variables: {
+          input: {
+            slug: this.data.value.slug,
+          },
+        },
+      });
+      this.router.transitionTo("reports");
+    } catch (error) {
+      console.error(error);
+      this.notification.danger(
+        this.intl.t(`caluma.analytics.notification.delete-error`)
+      );
+    }
   }
 }
--- packages/analytics/addon/controllers/reports/edit/index.js
+++ packages/analytics/addon/controllers/reports/edit/index.js
@@ -2,10 +2,9 @@ import Controller from "@ember/controller";
 import { inject as service } from "@ember/service";
 import { tracked } from "@glimmer/tracking";
 import { queryManager } from "ember-apollo-client";
-import { task, dropTask } from "ember-concurrency";
+import { task } from "ember-concurrency";
 import { trackedTask } from "ember-resources/util/ember-concurrency";
 
-import removeAnalyticsTableMutation from "@projectcaluma/ember-analytics/gql/mutations/remove-analytics-table.graphql";
 import getAnalyticsTable from "@projectcaluma/ember-analytics/tasks/get-analytics-table";
 
 export default class ReportsEditIndexController extends Controller {
@@ -15,25 +14,4 @@ export default class ReportsEditIndexController extends Controller {
 
   @task getTable = getAnalyticsTable;
   @tracked data = trackedTask(this, this.getTable, () => [this.model]);
-
-  @dropTask
-  *deleteTable() {
-    try {
-      yield this.apollo.mutate({
-        mutation: removeAnalyticsTableMutation,
-        fetchPolicy: "network-only",
-        variables: {
-          input: {
-            slug: this.data.value.slug,
-          },
-        },
-      });
-      this.router.transitionTo("reports");
-    } catch (error) {
-      console.error(error);
-      this.notification.danger(
-        this.intl.t(`caluma.analytics.notification.delete-error`)
-      );
-    }
-  }
 }
