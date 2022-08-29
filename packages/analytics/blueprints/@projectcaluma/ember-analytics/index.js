76113d0219123c3ab122eff560c79680a84c8357
Jonas Metzener <jonas.metzener@adfinis.com>
2022-08-29 17:52:11 +0200

    chore(deps): update dependencies

--- packages/analytics/blueprints/@projectcaluma/ember-analytics/index.js
+++ packages/analytics/blueprints/@projectcaluma/ember-analytics/index.js
@@ -7,10 +7,8 @@ module.exports = {
     return this.addAddonsToProject({
       packages: [
         { name: "@projectcaluma/ember-core" },
-        { name: "ember-cli-showdown" },
+        { name: "ember-engines" },
         { name: "ember-composable-helpers" },
-        { name: "ember-math-helpers" },
-        { name: "ember-pikaday" },
         { name: "ember-power-select" },
       ],
     });
