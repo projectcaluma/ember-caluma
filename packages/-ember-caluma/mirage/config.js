c860ce5ee48a52f7daf2721b786f714c37bb3bcb
Jonas Metzener <jonas.metzener@adfinis.com>
2022-09-09 13:55:05 +0200

    feat(distribution): update the inquiry meta reminder property after sending an inquiry

--- packages/-ember-caluma/mirage/config.js
+++ packages/-ember-caluma/mirage/config.js
@@ -1,5 +1,4 @@
 import { discoverEmberDataModels } from "ember-cli-mirage";
-import { DateTime } from "luxon";
 import { createServer, Response } from "miragejs";
 
 import graphqlHandler from "@projectcaluma/ember-testing/mirage-graphql";
@@ -41,16 +40,7 @@ export default function makeServer(config) {
         });
       });
 
-      this.post(":inquiryId/send-reminder", ({ workItems }, request) => {
-        const inquiry = workItems.find(request.params.inquiryId);
-        inquiry.update("meta", {
-          reminders: [
-            DateTime.now().toJSDate(),
-            ...(inquiry.meta.reminders ?? []),
-          ],
-        });
-        return new Response(200);
-      });
+      this.post(":inquiryId/send-reminder", 200);
 
       this.passthrough();
     },
