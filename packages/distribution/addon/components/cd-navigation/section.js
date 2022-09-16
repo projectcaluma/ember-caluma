81fb2cc276f321f524ee4b12f30c56a968768293
Jonas Metzener <jonas.metzener@adfinis.com>
2022-09-16 16:23:29 +0200

    feat(distribution): mark navigation section as active as well

--- packages/distribution/addon/components/cd-navigation/section.js
+++ packages/distribution/addon/components/cd-navigation/section.js
@@ -1,9 +1,23 @@
 import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 
 export default class CdNavigationSectionComponent extends Component {
+  @service router;
+
   @tracked expanded = true;
+
+  get isActive() {
+    return (
+      this.inquiries.find((inquiry) =>
+        this.router.isActive("inquiry", {
+          to: inquiry.addressedGroups[0],
+          from: inquiry.controllingGroups[0],
+        })
+      ) !== undefined
+    );
+  }
 
   @action
   toggle(e) {
@@ -18,8 +32,10 @@ export default class CdNavigationSectionComponent extends Component {
         ? "controllingGroupName"
         : "addressedGroupName";
 
-    return this.args.inquiries.sort((a, b) =>
-      a[sortProperty].localeCompare(b[sortProperty])
+    return (
+      this.args.inquiries?.sort((a, b) =>
+        a[sortProperty].localeCompare(b[sortProperty])
+      ) ?? []
     );
   }
 }
