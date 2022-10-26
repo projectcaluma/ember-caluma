7bbf7b01e0b63fe5f06c9fd63b1608aaf2f6b9e1
Alana Luyten <alana.luyten@adfinis.com>
2022-10-26 09:53:53 +0200

    fix(distribution): refetch inquiry after editing for dialog preview

--- packages/distribution/addon/components/cd-inquiry-edit-form.js
+++ packages/distribution/addon/components/cd-inquiry-edit-form.js
@@ -1,5 +1,6 @@
 import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
 import { queryManager } from "ember-apollo-client";
 import { dropTask } from "ember-concurrency";
 import { trackedTask } from "ember-resources/util/ember-concurrency";
@@ -7,15 +8,25 @@ import { trackedTask } from "ember-resources/util/ember-concurrency";
 import config from "@projectcaluma/ember-distribution/config";
 import resumeWorkItemMutation from "@projectcaluma/ember-distribution/gql/mutations/resume-work-item.graphql";
 import inquiryEditQuery from "@projectcaluma/ember-distribution/gql/queries/inquiry-edit.graphql";
+import inquiryQuery from "@projectcaluma/ember-distribution/gql/queries/inquiry.graphql";
 
 export default class CdInquiryEditFormComponent extends Component {
   @service notification;
   @service router;
   @service intl;
+  @tracked inquiryId;
 
   @config config;
 
   @queryManager apollo;
+
+  constructor(...args) {
+    // setting field is necessary to have access to the inquiry id
+    // in the willDestroy hook, since it doesn't seem to
+    // have access to args
+    super(...args);
+    this.inquiryId = this.args.inquiry;
+  }
 
   _inquiry = trackedTask(this, this.fetchInquiry, () => [this.args.inquiry]);
 
@@ -28,7 +39,11 @@ export default class CdInquiryEditFormComponent extends Component {
     return yield this.apollo.watchQuery(
       {
         query: inquiryEditQuery,
-        variables: { inquiry },
+        variables: {
+          inquiry,
+          infoQuestion: this.config.inquiry.infoQuestion,
+          deadlineQuestion: this.config.inquiry.deadlineQuestion,
+        },
       },
       "allWorkItems.edges"
     );
@@ -50,5 +65,19 @@ export default class CdInquiryEditFormComponent extends Component {
         this.intl.t("caluma.distribution.edit.send-error")
       );
     }
+  }
+
+  async willDestroy() {
+    super.willDestroy();
+
+    await this.apollo.query({
+      query: inquiryQuery,
+      variables: {
+        inquiry: this.inquiryId,
+        infoQuestion: this.config.inquiry.infoQuestion,
+        deadlineQuestion: this.config.inquiry.deadlineQuestion,
+      },
+      fetchPolicy: "network-only",
+    });
   }
 }
