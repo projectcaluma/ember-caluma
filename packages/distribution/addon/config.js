1944c09c7074714c74fbcd85df861abcab87e256
Jonas Metzener <jonas.metzener@adfinis.com>
2022-10-26 10:11:35 +0200

    fix(distribution): only show closed at in detail if inquiry is closed

--- packages/distribution/addon/config.js
+++ packages/distribution/addon/config.js
@@ -68,11 +68,15 @@ export default function config(target, property) {
                   value: inquiry.assignedUsers,
                   type: "user",
                 },
-                {
-                  label: "caluma.distribution.inquiry.closed-at",
-                  value: inquiry.closedAt,
-                  type: "date",
-                },
+                ...(inquiry.closedAt
+                  ? [
+                      {
+                        label: "caluma.distribution.inquiry.closed-at",
+                        value: inquiry.closedAt,
+                        type: "date",
+                      },
+                    ]
+                  : []),
               ],
             },
           },
