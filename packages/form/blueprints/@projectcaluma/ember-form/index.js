7f0173d05e12f75eeb0375b3072797d4a95feb55
Jonas Metzener <jonas.metzener@adfinis.com>
2023-04-17 13:19:20 +0200

    fix(addon): add required dependencies to addon blueprints

--- packages/form/blueprints/@projectcaluma/ember-form/index.js
+++ packages/form/blueprints/@projectcaluma/ember-form/index.js
@@ -4,15 +4,23 @@ module.exports = {
   normalizeEntityName() {},
 
   afterInstall() {
+    /**
+     * Automatically install all ember addons that expose helpers / components
+     * used in templates of the addon itself. Other dependencies that are only
+     * used in JS code don't need to be installed in the host app and therefore
+     * don't have to be included here.
+     */
     return this.addAddonsToProject({
       packages: [
         { name: "@projectcaluma/ember-core" },
+        { name: "ember-autoresize-modifier" },
         { name: "ember-cli-showdown" },
         { name: "ember-composable-helpers" },
-        { name: "ember-math-helpers" },
+        { name: "ember-concurrency" },
         { name: "ember-flatpickr" },
+        { name: "ember-in-viewport" },
+        { name: "ember-math-helpers" },
         { name: "ember-power-select" },
-        { name: "ember-autoresize-modifier" },
       ],
     });
   },
