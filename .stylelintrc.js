592b6e7f1a56e455c8699f2b29f574ca72a06006
Jonas Metzener <jonas.metzener@adfinis.com>
2023-04-27 12:04:03 +0200

    chore(deps): update dependencies
    BREAKING CHANGE: Remove support for Ember v3.

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = {
+  extends: ["stylelint-config-standard-scss", "stylelint-prettier/recommended"],
+  rules: {
+    "selector-class-pattern": null,
+    "scss/dollar-variable-pattern": null,
+  },
+};
--- config/ember-try.js
+++ config/ember-try.js
@@ -9,14 +9,6 @@ module.exports = async function () {
     usePnpm: true,
     scenarios: [
       {
-        name: "ember-lts-3.28",
-        npm: {
-          devDependencies: {
-            "ember-source": "~3.28.0",
-          },
-        },
-      },
-      {
         name: "ember-lts-4.4",
         npm: {
           devDependencies: {
@@ -53,24 +45,6 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             "ember-source": await getChannelURL("canary"),
-          },
-        },
-      },
-      {
-        name: "ember-classic",
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            "application-template-wrapper": true,
-            "default-async-observers": false,
-            "template-only-glimmer-components": false,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            "ember-source": "~3.28.0",
-          },
-          ember: {
-            edition: "classic",
           },
         },
       },
