7a1f682f4a02215160e6f2a2595b0652c702ed07
Jonas Metzener <jonas.metzener@adfinis.com>
2023-06-19 14:12:55 +0200

    chore(tests): fix confirm helper

--- packages/distribution/tests/helpers/confirm.js
+++ packages/distribution/tests/helpers/confirm.js
@@ -1,6 +1,20 @@
-import { click, waitFor } from "@ember/test-helpers";
+import { click, waitUntil } from "@ember/test-helpers";
+import UIkit from "uikit";
+
+function getModal() {
+  return UIkit.modal(".uk-modal.uk-open");
+}
 
 export default async function confirm() {
-  await waitFor(".uk-modal.uk-open");
-  await click(".uk-modal-footer .uk-button-primary");
+  // Wait until the confirm modal is in the DOM
+  await waitUntil(getModal);
+
+  // Wait until the confirm modal is done animating
+  const modal = getModal();
+  await waitUntil(() => !modal.hasAnimation);
+
+  // Click confirm button
+  await click(
+    modal.$el.querySelector(".uk-modal-footer .uk-button.uk-button-primary")
+  );
 }
