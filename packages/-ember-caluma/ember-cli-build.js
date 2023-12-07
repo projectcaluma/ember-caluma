4ad9a47800918e4b5c455a3010d51863436c6600
Jonas Metzener <jonas.metzener@adfinis.com>
2023-12-07 13:54:30 +0100

    chore: use native fetch

--- packages/-ember-caluma/ember-cli-build.js
+++ packages/-ember-caluma/ember-cli-build.js
@@ -8,6 +8,9 @@ module.exports = function (defaults) {
     "ember-cli-addon-docs": {
       documentingAddonAt: "../form",
     },
+    "ember-fetch": {
+      nativePromise: true,
+    },
   });
 
   return app.toTree();
