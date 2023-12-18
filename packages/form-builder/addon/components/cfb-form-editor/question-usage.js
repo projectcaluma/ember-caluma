ff887292ed2cc9f133ec32a8735bc7936f5c7e9e
Falk <falk.neumann@adfinis.com>
2023-12-18 17:23:45 +0100

    fix: incorporate review feedback

--- packages/form-builder/addon/components/cfb-form-editor/question-usage.js
+++ packages/form-builder/addon/components/cfb-form-editor/question-usage.js
@@ -1,3 +1,4 @@
+import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { queryManager } from "ember-apollo-client";
@@ -6,24 +7,40 @@ import { trackedFunction } from "ember-resources/util/function";
 import allFormsForQuestionQuery from "@projectcaluma/ember-form-builder/gql/queries/all-forms-for-question.graphql";
 
 export default class CfbFormEditorQuestionUsage extends Component {
-  @tracked modalVisible = false;
+  @service intl;
   @queryManager apollo;
 
+  @tracked modalVisible = false;
+
+  get title() {
+    return this.intl.t("caluma.form-builder.question.usage.title", {
+      n: this.otherForms,
+      // for highlighting the number we use the <b> tag
+      htmlSafe: true,
+    });
+  }
+
+  get otherForms() {
+    return this.forms.value?.length - 1 ?? 0;
+  }
+
   forms = trackedFunction(this, async () => {
-    if (!this.modalVisible) {
-      return null;
-    }
-
-    const forms = await this.apollo.query(
-      {
-        query: allFormsForQuestionQuery,
-        variables: {
-          slug: this.args.model.slug,
+    try {
+      const forms = await this.apollo.query(
+        {
+          query: allFormsForQuestionQuery,
+          variables: {
+            slug: this.args.model.slug,
+          },
+          fetchPolicy: "no-cache",
         },
-      },
-      "allForms.edges",
-    );
+        "allForms.edges",
+      );
 
-    return forms;
+      return forms;
+    } catch (error) {
+      console.error(error);
+      return { value: [] };
+    }
   });
 }
