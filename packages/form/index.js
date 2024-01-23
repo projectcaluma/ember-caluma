f395943b201a3fb32fb01dc6fdf3612baead8e09
Jonas Metzener <jonas.metzener@adfinis.com>
2024-01-23 10:15:54 +0100

    chore(flatpickr): fix usage of flatpickr

--- packages/form/index.js
+++ packages/form/index.js
@@ -2,16 +2,4 @@
 
 module.exports = {
   name: require("./package").name,
-
-  included(...args) {
-    const app = this._findHost(this);
-
-    app.options.flatpickr = {
-      locales: ["de", "fr"],
-      theme: "airbnb",
-      ...(app.options.flatpickr ?? {}),
-    };
-
-    this._super.included.apply(this, args);
-  },
 };
