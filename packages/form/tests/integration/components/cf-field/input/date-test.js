580371e4c13f6e17c21a68c648a941e9f5c96266
Jonas Metzener <jonas.metzener@adfinis.com>
2024-03-18 12:45:35 +0100

    fix(form): only pass locale string to flatpickr

--- packages/form/tests/integration/components/cf-field/input/date-test.js
+++ packages/form/tests/integration/components/cf-field/input/date-test.js
@@ -1,4 +1,4 @@
-import { fillIn, blur, render } from "@ember/test-helpers";
+import { fillIn, blur, render, waitFor } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
 import { setFlatpickrDate } from "ember-flatpickr/test-support/helpers";
 import { setupIntl, setLocale } from "ember-intl/test-support";
@@ -51,17 +51,17 @@ module("Integration | Component | cf-field/input/date", function (hooks) {
       hbs`<CfField::Input::Date @disabled={{true}} @field={{this.field}} />`,
     );
 
+    await waitFor(".ember-flatpickr-input.input");
+
+    assert.dom(".ember-flatpickr-input.input").hasAttribute("type", "text");
+    assert.dom(".ember-flatpickr-input.input").hasValue("10.09.2021");
     assert
-      .dom(".ember-flatpickr-input:not([type='hidden'])")
-      .hasAttribute("type", "text");
-    assert
-      .dom(".ember-flatpickr-input:not([type='hidden'])")
-      .hasValue("10.09.2021");
-    assert
-      .dom(".ember-flatpickr-input[type='hidden']")
+      .dom(".ember-flatpickr-input.flatpickr-input")
       .hasAttribute("readonly");
-    assert.dom(".ember-flatpickr-input[type='hidden']").hasClass("uk-disabled");
-    assert.dom(".ember-flatpickr-input[type='hidden']").hasValue("2021-09-10");
+    assert
+      .dom(".ember-flatpickr-input.flatpickr-input")
+      .hasClass("uk-disabled");
+    assert.dom(".ember-flatpickr-input.flatpickr-input").hasValue("2021-09-10");
   });
 
   test("it works on input", async function (assert) {
@@ -70,35 +70,29 @@ module("Integration | Component | cf-field/input/date", function (hooks) {
     await render(hbs`<CfField::Input::Date @onSave={{fn (mut this.value)}} />`);
 
     await setLocale("en-us");
-    await fillIn(".ember-flatpickr-input:not([type='hidden'])", "something");
+    await fillIn(".ember-flatpickr-input.input", "something");
     await blur();
     assert.strictEqual(this.value, null);
-    assert.dom(".ember-flatpickr-input:not([type='hidden'])").hasValue("");
-    assert.dom(".ember-flatpickr-input[type='hidden']").hasValue("");
+    assert.dom(".ember-flatpickr-input.input").hasValue("");
+    assert.dom(".ember-flatpickr-input.flatpickr-input").hasValue("");
 
-    await fillIn(".ember-flatpickr-input:not([type='hidden'])", "1/30/2013");
+    await fillIn(".ember-flatpickr-input.input", "1/30/2013");
     await blur();
     assert.strictEqual(this.value, "2013-01-30");
-    assert
-      .dom(".ember-flatpickr-input:not([type='hidden'])")
-      .hasValue("01/30/2013");
-    assert.dom(".ember-flatpickr-input[type='hidden']").hasValue("2013-01-30");
+    assert.dom(".ember-flatpickr-input.input").hasValue("01/30/2013");
+    assert.dom(".ember-flatpickr-input.flatpickr-input").hasValue("2013-01-30");
 
     await setLocale("de-ch");
-    await fillIn(".ember-flatpickr-input:not([type='hidden'])", "20.2.2022");
+    await fillIn(".ember-flatpickr-input.input", "20.2.2022");
     await blur();
     assert.strictEqual(this.value, "2022-02-20");
-    assert
-      .dom(".ember-flatpickr-input:not([type='hidden'])")
-      .hasValue("20.02.2022");
-    assert.dom(".ember-flatpickr-input[type='hidden']").hasValue("2022-02-20");
+    assert.dom(".ember-flatpickr-input.input").hasValue("20.02.2022");
+    assert.dom(".ember-flatpickr-input.flatpickr-input").hasValue("2022-02-20");
 
-    await fillIn(".ember-flatpickr-input:not([type='hidden'])", "25.03.2021");
+    await fillIn(".ember-flatpickr-input.input", "25.03.2021");
     await blur();
     assert.strictEqual(this.value, "2021-03-25");
-    assert
-      .dom(".ember-flatpickr-input:not([type='hidden'])")
-      .hasValue("25.03.2021");
-    assert.dom(".ember-flatpickr-input[type='hidden']").hasValue("2021-03-25");
+    assert.dom(".ember-flatpickr-input.input").hasValue("25.03.2021");
+    assert.dom(".ember-flatpickr-input.flatpickr-input").hasValue("2021-03-25");
   });
 });
