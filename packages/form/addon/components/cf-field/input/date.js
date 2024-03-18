edd3670d2b3d426f031dae5d808174a9f199cfee
Jonas Metzener <jonas.metzener@adfinis.com>
2024-03-18 12:45:35 +0100

    fix(form): only pass locale string to flatpickr

--- packages/form/addon/components/cf-field/input/date.js
+++ packages/form/addon/components/cf-field/input/date.js
@@ -3,7 +3,6 @@ import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
-import lang from "flatpickr/dist/l10n";
 import { DateTime } from "luxon";
 
 export default class CfFieldInputDateComponent extends Component {
@@ -13,10 +12,6 @@ export default class CfFieldInputDateComponent extends Component {
 
   get locale() {
     return this.intl.primaryLocale.split("-")[0];
-  }
-
-  get localeObject() {
-    return lang[this.locale];
   }
 
   get config() {
