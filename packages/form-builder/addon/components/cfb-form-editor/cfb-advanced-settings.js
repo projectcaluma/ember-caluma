b045dde9ae5adeae7213ea05844824714e768953
Stephan Hug <stephan.hug@adfinis.com>
2024-04-23 16:27:26 +0200

    feat(form-builder): save the preference for "show advanced" settings

--- packages/form-builder/addon/components/cfb-form-editor/cfb-advanced-settings.js
+++ packages/form-builder/addon/components/cfb-form-editor/cfb-advanced-settings.js
@@ -1,6 +1,20 @@
+import { action } from "@ember/object";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
-
 export default class CfbFormEditorCfbAdvancedSettings extends Component {
   @tracked showAdvanced = false;
+
+  constructor(owner, args) {
+    super(owner, args);
+
+    this.showAdvanced =
+      JSON.parse(localStorage.getItem("showAdvanced")) ?? false;
+  }
+
+  @action
+  toggleAdvanced() {
+    this.showAdvanced = !this.showAdvanced;
+
+    localStorage.setItem("showAdvanced", this.showAdvanced);
+  }
 }
