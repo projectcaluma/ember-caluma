97aec9899e9f64b12b2876053fe0727216ed9245
Niels Verbeek <niels.verbeek@adfinis.com>
2025-08-14 13:52:52 +0200

    Revert "chore(jexl): revert JEXL evaluation condition"
    This reverts commit a4cdefdc563763555df9decb8ef07f7d2d5474dd.

--- packages/form/addon/lib/field.js
+++ packages/form/addon/lib/field.js
@@ -528,7 +528,7 @@ export default class Field extends Base {
     if (
       this.fieldset.field?.hidden ||
       (this.hiddenDependencies.length &&
-        this.hiddenDependencies.every(fieldIsHiddenOrEmpty))
+        this.hiddenDependencies.every(fieldIsHidden))
     ) {
       return true;
     }
