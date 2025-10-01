eb535b813138db4e4ac493d9000f9d91eb5e4eb1
Jonas Metzener <jonas.metzener@adfinis.com>
2025-10-01 17:15:08 +0200

    wip

--- packages/distribution/addon/components/cd-navigation/item.js
+++ packages/distribution/addon/components/cd-navigation/item.js
@@ -11,6 +11,13 @@ export default class CdNavigationItemComponent extends Component {
     };
   }
 
+  get showPendingIndicator() {
+    return (
+      this.args.inquiry.totalCount > 1 &&
+      this.args.inquiry.totalCount !== this.args.inquiry.answeredCount
+    );
+  }
+
   get isActive() {
     return this.router.isActive("inquiry", this.model);
   }
