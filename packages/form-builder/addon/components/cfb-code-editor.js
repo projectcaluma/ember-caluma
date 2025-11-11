f06cde147157a3fbe58e1dbfeb16ea76b56e10d1
Jonas Metzener <jonas.metzener@adfinis.com>
2025-11-11 13:18:55 +0100

    fix(code-editor): various fixes for the code editor (jexl / json)
    - Update CodeJar to the latest version which should fix the cursor
      behaviour in Firefox
    - Debounce update function called on the observer to avoid multiple
      updates and loops
    - Make sure to only update values if they changed by simplifying the
      check between current and last value

--- packages/form-builder/addon/components/cfb-code-editor.js
+++ packages/form-builder/addon/components/cfb-code-editor.js
@@ -2,6 +2,7 @@ import { action } from "@ember/object";
 import { addObserver } from "@ember/object/observers";
 import Component from "@glimmer/component";
 import { CodeJar } from "codejar";
+import { task, timeout } from "ember-concurrency";
 import hljs from "highlight.js/lib/core";
 import json from "highlight.js/lib/languages/json";
 import markdown from "highlight.js/lib/languages/markdown";
@@ -15,20 +16,32 @@ hljs.registerLanguage("json", json);
 hljs.registerLanguage("markdown", markdown);
 hljs.registerLanguage("jexl", jexl);
 
+const simplify = (value) => {
+  if (typeof value === "object") {
+    value = JSON.stringify(value?.unwrap?.() ?? value);
+  }
+
+  return value?.replace(/\s/g, "");
+};
+
 export default class CfbCodeEditorComponent extends Component {
   _editor = null;
-  _lastValue = this.args.value;
+  _lastValue = null;
 
   get stringValue() {
     if (this.args.language === "json" && typeof this.args.value === "object") {
       return JSON.stringify(
         this.args.value?.unwrap?.() ?? this.args.value,
         null,
-        2,
+        "\t",
       );
     }
 
     return this.args.value;
+  }
+
+  _didChange(value) {
+    return !isEqual(this._lastValue, simplify(value));
   }
 
   @action
@@ -41,24 +54,22 @@ export default class CfbCodeEditorComponent extends Component {
       }
     }
 
-    if (isEqual(this._lastValue, value)) return;
+    if (!this._didChange(value)) return;
 
-    this._lastValue = value;
+    this._lastValue = simplify(value);
     this.args.update(value);
   }
 
-  @action
-  updateCode() {
-    if (isEqual(this._lastValue, this.args.value)) return;
+  updateEditorContent = task({ restartable: true }, async () => {
+    // This function is called everytime `this.args.value` changes. In order to
+    // not trigger too many updates, we debounce it for 10ms.
+    await timeout(10);
 
+    if (!this._didChange(this.args.value)) return;
+
+    this._lastValue = simplify(this.args.value);
     this._editor.updateCode(this.stringValue, false);
-  }
-
-  @action
-  onBlur() {
-    this._editor.updateCode(this.stringValue);
-    this.args.setDirty();
-  }
+  });
 
   @action
   didInsertNode(element) {
@@ -68,13 +79,14 @@ export default class CfbCodeEditorComponent extends Component {
     });
 
     // set initial value
-    this._editor.updateCode(this.stringValue);
+    this._editor.updateCode(this.stringValue, false);
+    this._lastValue = simplify(this.args.value);
 
     // register update method
     this._editor.onUpdate(this.onUpdate);
 
     // eslint-disable-next-line ember/no-observers
-    addObserver(this.args, "value", this, "updateCode");
+    addObserver(this.args, "value", this.updateEditorContent, "perform");
   }
 
   @action
