14545010c5596f285a9fbf7f2af6e6bf28ba804d
Jonas Metzener <jonas.metzener@adfinis.com>
2025-11-11 13:18:55 +0100

    fix(code-editor): various fixes for the code editor (jexl / json)
    - Update CodeJar to the latest version which should fix the cursor
      behaviour in Firefox
    - Debounce update function called on the observer to avoid multiple
      updates and loops
    - Make sure to only update values if they changed by simplifying the
      check between current and last value

--- packages/form-builder/tests/integration/components/cfb-code-editor-test.js
+++ packages/form-builder/tests/integration/components/cfb-code-editor-test.js
@@ -1,5 +1,6 @@
 import { render, typeIn, fillIn, settled, blur } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
+import { timeout } from "ember-concurrency";
 import { module, test } from "qunit";
 
 import { setupRenderingTest } from "dummy/tests/helpers";
@@ -22,6 +23,7 @@ module("Integration | Component | cfb-code-editor", function (hooks) {
     // use `typeIn` because codejar listens to keydown event
     await typeIn("[name=editor]", "1 + 1");
     await blur("[name=editor]");
+    await timeout(500);
 
     // syntax highlighting creates child <span>s
     assert.dom("[name=editor] span.hljs-number").exists({ count: 2 });
@@ -71,6 +73,7 @@ module("Integration | Component | cfb-code-editor", function (hooks) {
     await fillIn("[name=editor]", `{"bar":"baz"}`);
     await typeIn("[name=editor]", " ");
     await blur("[name=editor]");
+    await timeout(500);
 
     assert.deepEqual(this.value, { bar: "baz" });
     assert.true(this.dirty);
