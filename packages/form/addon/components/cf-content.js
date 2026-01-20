72e09fad371021af6a6dccbd7d361fd0588f742a
Niels Verbeek <niels.verbeek@adfinis.com>
2026-01-20 09:41:23 +0100

    feat(compare): merge content-compare into compare component

--- packages/form/addon/components/cf-content.js
+++ packages/form/addon/components/cf-content.js
@@ -6,6 +6,7 @@ import { queryManager } from "ember-apollo-client";
 import { task } from "ember-concurrency";
 import { trackedTask } from "reactiveweb/ember-concurrency";
 
+import getDocumentAnswersCompareQuery from "@projectcaluma/ember-form/gql/queries/document-answers-compare.graphql";
 import getDocumentAnswersQuery from "@projectcaluma/ember-form/gql/queries/document-answers.graphql";
 import getDocumentFormsQuery from "@projectcaluma/ember-form/gql/queries/document-forms.graphql";
 import { parseDocument } from "@projectcaluma/ember-form/lib/parsers";
@@ -30,6 +31,12 @@ import { parseDocument } from "@projectcaluma/ember-form/lib/parsers";
  *   </div>
  * </CfContent>
  * ```
+ * To use this component for a comparison between two document revisions, pass @compare
+ *
+ * <CfContent @documentId="some-id" @compare={{hash
+      from="yyyy-mm-dd"
+      to="yyyy-mm-dd"
+ * }} />
  *
  * If you're rendering multi-page forms, the component also expects a query
  * param `displayedForm`:
@@ -70,6 +77,13 @@ export default class CfContentComponent extends Component {
    * Whether the form renders in disabled state
    *
    * @argument {Boolean} disabled
+   */
+
+  /**
+   * Can be used to pass "compare" context information from the outside.
+   * Pass a hash with `from` and optional `to` date to compare documents.
+   *
+   * @argument {Object} compare
    */
 
   /**
@@ -131,38 +145,65 @@ export default class CfContentComponent extends Component {
 
     if (!this.args.documentId) return;
 
-    const [answerDocument] = (
-      await this.apollo.query(
+    const owner = getOwner(this);
+    const Document = owner.factoryFor("caluma-model:document").class;
+    const Navigation = owner.factoryFor("caluma-model:navigation").class;
+
+    let answerDocument = null;
+    let historicalDocument = null;
+    let compareContext = null;
+
+    if (!this.args.compare) {
+      [answerDocument] = (await this.apollo.query(
         {
           query: getDocumentAnswersQuery,
           fetchPolicy: "network-only",
           variables: { id: this.args.documentId },
         },
         "allDocuments.edges",
-      )
-    ).map(({ node }) => node);
+      )).map(({ node }) => node);
+    } else {
+      compareContext = { enabled: true, ...this.args.compare };
+      const { from, to } = compareContext;
 
-    const [form] = (
-      await this.apollo.query(
-        {
-          query: getDocumentFormsQuery,
-          fetchPolicy: "cache-first",
-          variables: { slug: answerDocument.form.slug },
+      function toDate(input) {
+        if (input instanceof Date) {
+          return new Date(input.getTime());
+        }
+
+        return new Date(input);
+      }
+      const data = await this.apollo.query({
+        query: getDocumentAnswersCompareQuery,
+        fetchPolicy: "network-only",
+        variables: {
+          documentId: this.args.documentId,
+          from: toDate(from),
+          to: toDate(to ?? new Date()),
         },
-        "allForms.edges",
-      )
-    ).map(({ node }) => node);
+      });
+      historicalDocument = data.fromRevision;
+      answerDocument = data.toRevision;
+    }
 
-    const owner = getOwner(this);
-    const Document = owner.factoryFor("caluma-model:document").class;
-    const Navigation = owner.factoryFor("caluma-model:navigation").class;
+    const [form] = (await this.apollo.query(
+      {
+        query: getDocumentFormsQuery,
+        fetchPolicy: "cache-first",
+        variables: { slug: answerDocument.form.slug },
+      },
+      "allForms.edges",
+    )).map(({ node }) => node);
 
     const raw = parseDocument({ ...answerDocument, form });
 
     const document = new Document({
       raw,
       owner,
-      dataSourceContext: this.args.context,
+      historicalDocument: historicalDocument
+        ? parseDocument({ ...historicalDocument, form })
+        : null,
+      compare: compareContext,
     });
     const navigation = new Navigation({ document, owner });
 
