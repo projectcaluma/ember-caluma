e08dfdbf20b55eabc12dc5eedb9136326522724f
Niels Verbeek <niels.verbeek@adfinis.com>
2026-01-23 14:40:41 +0100

    feat(compare): use simple class helper utils

--- packages/form/addon/components/cf-field/input/radio.js
+++ packages/form/addon/components/cf-field/input/radio.js
@@ -2,6 +2,21 @@ import { action } from "@ember/object";
 import Component from "@glimmer/component";
 
 export default class CfFieldInpuRadio extends Component {
+  isAnswerRemoved = (option) => {
+    return (
+      this.args.field.compare &&
+      this.args.field?.answer?.historicalValue === option.slug &&
+      this.args.field?.answer?.value !== option.slug
+    );
+  };
+  isAnswerAdded = (option) => {
+    return (
+      this.args.field.compare &&
+      this.args.field?.answer?.historicalValue !== option.slug &&
+      this.args.field?.answer?.value === option.slug
+    );
+  };
+
   @action
   reset(event) {
     event.preventDefault();
