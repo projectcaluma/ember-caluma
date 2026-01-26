3525914a9f60757493dcddb2c11b42435a41035c
Jonas Metzener <jonas.metzener@adfinis.com>
2026-01-26 15:15:52 +0100

    chore(deps): update ember

--- packages/distribution/tests/dummy/app/app.js
+++ packages/distribution/tests/dummy/app/app.js
@@ -1,9 +1,14 @@
 import Application from "@ember/application";
+import { importSync, isDevelopingApp, macroCondition } from "@embroider/macros";
 import { extendResolver } from "ember-can";
 import loadInitializers from "ember-load-initializers";
 import Resolver from "ember-resolver";
 
 import config from "dummy/config/environment";
+
+if (macroCondition(isDevelopingApp())) {
+  importSync("./deprecation-workflow");
+}
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
