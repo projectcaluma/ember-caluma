ca3c12b495ee9ea035b02fe16b1d02526c8b3be1
Jonas Lehmann <jonas.lehmann@adfinis.com>
2026-04-29 16:41:06 +0200

    fix(number-separator): don't allow not wanted input for integer and float questions
    When a user adds a comma in a integer or float question everything after
    the comma and the comma itself is getting stripped from the original
    input. But the user can't see that because it does only show after a
    reload. Because of that we don't allow unwanted input in those fields
    anymore. If the user enters a not allowed character 6 times, a
    notification shows up.

--- packages/form/addon/components/cf-field/input/number-separator.js
+++ packages/form/addon/components/cf-field/input/number-separator.js
@@ -5,6 +5,10 @@ import { cached } from "tracked-toolbox";
 
 export default class CfFieldInputNumberSeparatorComponent extends Component {
   @service intl;
+  @service notification;
+
+  blockCount = 0;
+  blockNotified = false;
 
   get disabled() {
     return this.args.disabled || this.args.field?.question.isCalculated;
@@ -38,11 +42,58 @@ export default class CfFieldInputNumberSeparatorComponent extends Component {
     return this.intl.formatNumber(1.1).replace(/\p{Number}/gu, "");
   }
 
+  @cached
+  get questionType() {
+    return this.args.field?.question?.raw?.__typename;
+  }
+
+  @cached
+  get allowedRegex() {
+    const mil = RegExp.escape(this.thousandSeparator);
+    const dec = RegExp.escape(this.decimalSeparator);
+
+    if (this.questionType === "IntegerQuestion") {
+      return new RegExp(`(\\d|${mil})`);
+    }
+    return new RegExp(`(\\d|${mil}|${dec})`);
+  }
+
+  @action
+  checkInput(event) {
+    // allow keys like backspace, tab and arrows
+    if (event.key.length > 1 || event.ctrlKey || event.metaKey) {
+      return;
+    }
+
+    if (!this.allowedRegex.test(event.key)) {
+      event.preventDefault();
+      this.blockCount += 1;
+    }
+
+    if (this.blockCount > 5 && !this.blockNotified) {
+      this.questionType === "IntegerQuestion"
+        ? this.notification.warning(
+            this.intl.t(`caluma.form.validation.blockNotification`, {
+              allowedChars: `0-9 ${this.thousandSeparator}`,
+            }),
+          )
+        : this.notification.warning(
+            this.intl.t(`caluma.form.validation.blockNotification`, {
+              allowedChars: `0-9 ${this.thousandSeparator} ${this.decimalSeparator}`,
+            }),
+          );
+
+      this.blockCount = 0;
+      this.blockNotified = true;
+    }
+  }
+
   @action
   input({ target: { value } }) {
     // We need to remove the thousand separator and replace the decimal
     // separator with a dot in order to parse it into a number. Which character
     // those are is determined per locale in the getters above.
+
     const serialized = parseFloat(
       value
         .replace(new RegExp(`\\${this.thousandSeparator}`, "g"), "")
--- packages/form/tests/integration/components/cf-field/input/number-separator-test.js
+++ packages/form/tests/integration/components/cf-field/input/number-separator-test.js
@@ -1,8 +1,9 @@
-import { fillIn, render } from "@ember/test-helpers";
+import { fillIn, render, triggerKeyEvent } from "@ember/test-helpers";
 import { tracked } from "@glimmer/tracking";
 import { hbs } from "ember-cli-htmlbars";
 import { setLocale } from "ember-intl/test-support";
 import { module, test } from "qunit";
+import { stub } from "sinon";
 
 import { setupRenderingTest } from "dummy/tests/helpers";
 
@@ -80,6 +81,81 @@ module(
 
       assert.strictEqual(this.field.value, 1234.123);
       assert.dom("input").hasValue("1,234.123");
+    });
+
+    test("it blocks invalid characters for integer questions", async function (assert) {
+      await render(
+        hbs`<CfField::Input::NumberSeparator @field={{this.field}} @onSave={{this.save}} />`,
+      );
+
+      await fillIn("input", "10");
+      assert.dom("input").hasValue("10");
+
+      await triggerKeyEvent("input", "keydown", "A");
+      assert.dom("input").hasValue("10");
+
+      await triggerKeyEvent("input", "keydown", ",");
+      assert.dom("input").hasValue("10");
+
+      await triggerKeyEvent("input", "keydown", ".");
+      assert.dom("input").hasValue("10");
+    });
+
+    test("it blocks invalid characters for float questions", async function (assert) {
+      this.field.questionType = "FloatQuestion";
+
+      await render(
+        hbs`<CfField::Input::NumberSeparator @field={{this.field}} @onSave={{this.save}} />`,
+      );
+
+      await fillIn("input", "10.55");
+      assert.dom("input").hasValue("10.55");
+
+      await triggerKeyEvent("input", "keydown", "A");
+      assert.dom("input").hasValue("10.55");
+
+      await triggerKeyEvent("input", "keydown", ",");
+      assert.dom("input").hasValue("10.55");
+
+      await triggerKeyEvent("input", "keydown", ".");
+      assert.dom("input").hasValue("10.55");
+    });
+
+    test("triggers a warning exactly once after 6 attempts", async function (assert) {
+      const service = this.owner.lookup("service:notification");
+      const warning = stub(service, "warning");
+
+      await render(
+        hbs`<CfField::Input::NumberSeparator @field={{this.field}} @onSave={{this.save}} />`,
+      );
+
+      assert.ok(warning.notCalled);
+
+      await triggerKeyEvent("input", "keydown", ",");
+      await triggerKeyEvent("input", "keydown", ",");
+      await triggerKeyEvent("input", "keydown", ",");
+      await triggerKeyEvent("input", "keydown", ",");
+      await triggerKeyEvent("input", "keydown", ",");
+      // this one should trigger the notification
+      await triggerKeyEvent("input", "keydown", ",");
+
+      assert.ok(warning.called);
+      assert.ok(warning.calledOnce);
+      assert.strictEqual(
+        warning.args[0][0],
+        "In diesem Feld sind nur folgende Zeichen erlaubt: 0-9 ’ .",
+      );
+
+      await triggerKeyEvent("input", "keydown", ",");
+      await triggerKeyEvent("input", "keydown", ",");
+      await triggerKeyEvent("input", "keydown", ",");
+      await triggerKeyEvent("input", "keydown", ",");
+      await triggerKeyEvent("input", "keydown", ",");
+      // this one does not trigger the notification again
+      await triggerKeyEvent("input", "keydown", ",");
+
+      assert.ok(warning.calledOnce);
+      assert.notOk(warning.calledTwice);
     });
   },
 );
