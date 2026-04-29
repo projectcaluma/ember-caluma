ec06cbbac47e346546ac2f7852a61ddccdd2245c
Jonas Lehmann <jonas.lehmann@adfinis.com>
2026-04-29 16:41:06 +0200

    fix(number-separator): don't allow not wanted input for integer and float questions
    When a user adds a comma in a integer or float question everything after
    the comma and the comma itself is getting stripped from the original
    input. But the user can't see that because it does only show after a
    reload. Because of that we don't allow unwanted input in those fields
    anymore. If the user enters a not allowed character 6 times, a
    notification shows up.

--- packages/form/addon/components/cf-field/input/number-separator.js
+++ packages/form/addon/components/cf-field/input/number-separator.js
@@ -5,6 +5,10 @@ import { cached } from "tracked-toolbox";
 
 export default class CfFieldInputNumberSeparatorComponent extends Component {
   @service intl;
+  @service notification;
+
+  blockCount = 0;
+  blockNotified = false;
 
   get disabled() {
     return this.args.disabled || this.args.field?.question.isCalculated;
@@ -38,11 +42,57 @@ export default class CfFieldInputNumberSeparatorComponent extends Component {
     return this.intl.formatNumber(1.1).replace(/\p{Number}/gu, "");
   }
 
+  @cached
+  get allowedRegex() {
+    // Returns a regex for allowed input characters: digits and thousands
+    // separators for integers, plus decimal separators for floats.
+    const mil = RegExp.escape(this.thousandSeparator);
+    const dec = RegExp.escape(this.decimalSeparator);
+
+    if (this.args.field.questionType === "IntegerQuestion") {
+      return new RegExp(`(\\d|${mil})`);
+    }
+    return new RegExp(`(\\d|${mil}|${dec})`);
+  }
+
+  @action
+  checkInput(event) {
+    // allow keys like backspace, tab and arrows
+    if (event.key.length > 1 || event.ctrlKey || event.metaKey) {
+      return;
+    }
+
+    if (!this.allowedRegex.test(event.key)) {
+      event.preventDefault();
+      this.blockCount += 1;
+    }
+
+    // After the sixth attempt to enter a invalid character, the user will
+    // see a warning listing the characters that are allowed.
+    if (this.blockCount > 5 && !this.blockNotified) {
+      const allowedChars = ["0-9", this.thousandSeparator];
+
+      if (this.args.field.questionType === "FloatQuestion") {
+        allowedChars.push(this.decimalSeparator);
+      }
+
+      this.notification.warning(
+        this.intl.t(`caluma.form.validation.blockNotification`, {
+          allowedChars: allowedChars.join(" "),
+        }),
+      );
+
+      this.blockCount = 0;
+      this.blockNotified = true;
+    }
+  }
+
   @action
   input({ target: { value } }) {
     // We need to remove the thousand separator and replace the decimal
     // separator with a dot in order to parse it into a number. Which character
     // those are is determined per locale in the getters above.
+
     const serialized = parseFloat(
       value
         .replace(new RegExp(`\\${this.thousandSeparator}`, "g"), "")
