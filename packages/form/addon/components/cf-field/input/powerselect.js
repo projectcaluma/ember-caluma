7d363318ab20f7649204d67589d652c2c2dfaa66
Dawn Hwang <hwangh95@gmail.com>
2026-08-06 14:56:20 +0200

    chore: remove deprecated PowerSelectMultiple

--- packages/form/addon/components/cf-field/input/powerselect.js
+++ packages/form/addon/components/cf-field/input/powerselect.js
@@ -3,7 +3,6 @@ import { inject as service } from "@ember/service";
 import { ensureSafeComponent } from "@embroider/util";
 import Component from "@glimmer/component";
 import PowerSelectComponent from "ember-power-select/components/power-select";
-import PowerSelectMultipleComponent from "ember-power-select/components/power-select-multiple";
 
 import getConfig from "@projectcaluma/ember-core/utils/get-config";
 
@@ -33,10 +32,7 @@ export default class CfFieldInputPowerselectComponent extends Component {
   }
 
   get selectComponent() {
-    return ensureSafeComponent(
-      this.multiple ? PowerSelectMultipleComponent : PowerSelectComponent,
-      this,
-    );
+    return ensureSafeComponent(PowerSelectComponent, this);
   }
 
   get searchEnabled() {
