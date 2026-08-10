cca57ae26eabf6edda60168b5c3fcc3bdf1a00f3
Jonas Metzener <jonas.metzener@adfinis.com>
2026-08-10 13:46:06 +0200

    ci: allow expected pipeline failures

--- config/ember-try.js
+++ config/ember-try.js
@@ -63,8 +63,8 @@ module.exports = async function () {
           },
         },
       },
-      embroiderSafe({ allowedToFail: true }),
-      embroiderOptimized({ allowedToFail: true }),
+      embroiderSafe(),
+      embroiderOptimized(),
     ],
   };
 };
